fc9e7784127efb9b1dddd990281a8163e547d4c5
twoo0220 <twoo0220@gmail.com>
2000-12-30 10:41:00 +0000

    24.12.31 - arithmetic, string, parsing

---
+++ BOJ/C++/10822.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    std::string text;
+    std::cin >> text;
+
+    int size = text.size();
+    int number(0), sum(0);
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = i; i < size; ++i)
+        {
+            if (text[i] == ',')
+            {
+                break;
+            }
+            number *= 10;
+            number += text[i] - 48;
+        }
+        sum += number;
+        number = 0;
+    }
+
+    std::cout << sum;
+    return 0;
+}
