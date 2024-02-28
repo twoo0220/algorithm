c50088e7d8b681d60b5bcdf9b77d4c74a5703e49
twoo0220 <twoo0220@gmail.com>
2024-02-29 01:29:05 +0900

    24.02.29 - 25304

---
+++ BOJ/C++/25304.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	int receipt = 0;
+	std::cin >> receipt;
+
+	int type = 0;
+	std::cin >> type;
+
+	int total = 0;
+	for (int i = 0; i < type; ++i)
+	{
+		int price, count;
+		std::cin >> price >> count;
+		total += price * count;
+	}
+
+	if (receipt == total)
+	{
+		std::cout << "Yes" << std::endl;
+	}
+	else
+	{
+		std::cout << "No" << std::endl;
+	}
+
+	return 0;
+}
