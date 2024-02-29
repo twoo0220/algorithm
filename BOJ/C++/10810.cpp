fd12d6277dd7cf1bb5363968fd314008a66430e0
twoo0220 <twoo0220@gmail.com>
2024-03-01 02:06:29 +0900

    24.03.01 - 10810

---
+++ BOJ/C++/10810.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	int size(0), count(0);
+	std::cin >> size >> count;
+
+	int* basket = new int[size] {};
+	for (int i = 0; i < count; ++i)
+	{
+		int start(0), end(0), number(0);
+		std::cin >> start >> end >> number;
+
+		for (int j = start - 1; j < end; ++j)
+		{
+			basket[j] = number;
+		}
+	}
+
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << basket[i] << " ";
+	}
+
+	delete[] basket;
+	basket = nullptr;
+
+	return 0;
+}
