8a9211bdebbabdcb7da5a2b87faf15d476448caa
twoo0220 <twoo0220@gmail.com>
2024-03-01 01:03:23 +0900

    24.03.01 - 2908

---
+++ BOJ/C++/2908.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a(0), b(0);
+	std::cin >> a >> b;
+
+	int reverseA(0), reverseB(0);
+	for (int i = 100; i > 0; i /= 10)
+	{
+		reverseA += a % 10 * i;
+		a /= 10;
+
+		reverseB += b % 10 * i;
+		b /= 10;
+	}
+
+	if (reverseA > reverseB)
+	{
+		std::cout << reverseA;
+	}
+	else
+	{
+		std::cout << reverseB;
+	}
+
+	return 0;
+}
