222d06a5a07caf841b5decf37abe23ef3f32604e
twoo0220 <twoo0220@gmail.com>
2024-03-01 00:09:06 +0900

    24.03.01 - 25314

---
+++ BOJ/C++/25314.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	int count = 0;
+	std::cin >> count;
+
+	int share = count / 4 - 1;
+	
+	for (int i = 0; i < share; ++i)
+	{
+		std::cout << "long ";
+	}
+	std::cout << "long int";
+
+	return 0;
+}
