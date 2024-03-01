9b5d3506023749a5ba5f29684c68d5b3279fde04
twoo0220 <twoo0220@gmail.com>
2024-03-02 03:13:17 +0900

    24.03.02 - 1157

---
+++ BOJ/C++/1157.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string temp;
+	std::cin >> temp;
+
+	int arr[26]{};
+	for (size_t i = 0; i < temp.size(); ++i)
+	{
+		if (static_cast<int>(temp[i]) < 97)
+		{
+			arr[static_cast<int>(temp[i]) - 65]++;
+		}
+		else
+		{
+			arr[static_cast<int>(temp[i]) - 97]++;
+		}
+	}
+
+	int maxValue = 0;
+	int maxIndex = 0;
+	for (int i = 0; i < 26; ++i)
+	{
+		if (arr[i] == maxValue)
+		{
+			maxIndex = -2;
+		}
+		else if (arr[i] > maxValue)
+		{
+			maxValue = arr[i];
+			maxIndex = i;
+		}
+	}
+
+	std::cout << static_cast<char>(maxIndex + 65);
+	return 0;
+}
