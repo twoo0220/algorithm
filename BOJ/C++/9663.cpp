4e41a97aec3c74cf3d64c444b302af6845ac7e9e
twoo0220 <twoo0220@gmail.com>
2024-03-03 01:48:29 +0900

    24.03.03 - 9663

---
+++ BOJ/C++/9663.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int size = 0;
+int checkNumber = 0;
+int check[15]{};
+
+void queen(int count)
+{
+	if (count == size)
+	{
+		checkNumber++;
+	}
+	else
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			check[count] = i;
+			bool enable = true;
+			for (int j = 0; j < count; ++j)
+			{
+				if ((check[count] == check[j]) || ((count - j) == std::abs(check[count] - check[j])))
+				{
+					enable = false;
+					break;
+				}
+			}
+			if (enable == true)
+			{
+				queen(count + 1);
+			}
+		}
+	}
+}
+
+int main()
+{
+	std::cin >> size;
+	queen(0);
+	std::cout << checkNumber;
+
+	return 0;
+}
