be07a48949a91c8c9c0b3d1ca6e252f275e43e98
twoo0220 <twoo0220@gmail.com>
2024-03-03 00:16:43 +0900

    24.03.03

---
+++ BOJ/C++/1929.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	int m(1), n(1);
+	std::cin >> m >> n;
+
+	std::vector<bool> primeVec(n + 1, true);
+	primeVec[0] = false;
+	primeVec[1] = false;
+
+	for (int i = 2; i*i <= n; ++i)
+	{
+		if (!primeVec[i])
+			continue;
+
+		for (int j = i * i; j <= n; j += i)
+		{
+			primeVec[j] = false;
+		}
+	}
+
+	for (int i = m; i < n + 1; ++i)
+	{
+		if (primeVec[i])
+		{
+			std::cout << i << "\n";
+		}
+	}
+	return 0;
+}
---
+++ BOJ/C++/5597.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	bool task[30]{};
+	for (int i = 0; i < 28; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		task[temp - 1] = true;
+	}
+
+	for (int i = 0; i < 30; ++i)
+	{
+		if (task[i] == false)
+		{
+			std::cout << (i + 1) << "\n";
+		}
+	}
+
+	return 0;
+}
