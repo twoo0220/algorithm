57fdfca433c9991e15311bd95c3578c2622c80e5
twoo0220 <twoo0220@gmail.com>
2024-03-03 14:21:26 +0900

    24.03.03

---
+++ BOJ/C++/1978.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<bool> primeVec(1001, true);
+	primeVec[0] = false;
+	primeVec[1] = false;
+
+	for (int i = 2; i * i <= 1000; ++i)
+	{
+		if (!primeVec[i])
+			continue;
+
+		for (int j = i * i; j <= 1000; j += i)
+		{
+			primeVec[j] = false;
+		}
+	}
+
+	int size(0);
+	std::cin >> size;
+	int count = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		int temp = 0;
+		std::cin >> temp;
+		if (primeVec[temp] == true)
+		{
+			count++;
+		}
+	}
+
+	std::cout << count;
+	return 0;
+}
---
+++ BOJ/C++/2750.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int size = 0;
+    std::cin >> size;
+    
+    bool arr[2001]{};
+    for (int i = 0; i < size; ++i)
+    {
+        int temp = 0;
+        std::cin >> temp;
+        arr[temp + 1000] = true;
+    }
+
+    for (int i = 0; i < 2001; ++i)
+    {
+        if (arr[i] == true)
+        {
+            std::cout << (i - 1000) << "\n";
+        }
+    }
+
+    return 0;
+}
