2132c205be1edaa5896e05e67700a6ed0d97700f
twoo0220 <twoo0220@gmail.com>
2024-03-05 00:50:31 +0900

    24.03.05 - 2745

---
+++ BOJ/C++/2745.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	std::string temp;
+	int number(0);
+	std::cin >> temp >> number;
+
+	int result(0);
+	int end = temp.size();
+	for (int i = end; i > 0; --i)
+	{
+		if (static_cast<int>(temp[i - 1]) > 64)
+		{
+			result += (static_cast<int>(temp[i - 1]) - 55) * pow(number, end - i);
+		}
+		else
+		{
+			result += (static_cast<int>(temp[i - 1]) - 48) * pow(number, end - i);
+		}
+	}
+	std::cout << result;
+	return 0;
+}
