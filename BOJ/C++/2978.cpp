b255847da63b69c0d9bcc111b0a5252285fc08df
twoo0220 <twoo0220@gmail.com>
2024-03-07 01:39:46 +0900

    24.03.07 - 2978

---
+++ BOJ/C++/2978.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	int count(0), maxSize(0);
+	std::cin >> count >> maxSize;
+	int* card = new int[count];
+	for (int i = 0; i < count; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		card[i] = temp;
+	}
+
+	int max = 0;
+	for (int i = 0; i < count; ++i)
+	{
+		for (int j = i + 1; j < count; ++j)
+		{
+			for (int k = j + 1; k < count; ++k)
+			{
+				int maxTemp = card[i] + card[j] + card[k];
+				if (maxTemp <= maxSize)
+				{
+					max = maxTemp > max ? maxTemp : max;
+				}
+			}
+		}
+	}
+
+	std::cout << max;
+
+	delete[] card;
+	card = nullptr;
+	return 0;
+}
