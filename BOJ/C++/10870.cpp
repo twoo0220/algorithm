f76aefdf94d6ff6a6e932dbd3fbbd46a9e9f0a40
twoo0220 <twoo0220@gmail.com>
2024-03-09 00:50:11 +0900

    24.03.09

---
+++ BOJ/C++/10870.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int fibonacci(int number)
+{
+	if ((number == 0) || (number == 1))
+	{
+		return number;
+	}
+
+	return (fibonacci(number - 2) + fibonacci(number - 1));
+}
+
+int main()
+{
+	int number(0);
+	std::cin >> number;
+
+	std::cout << fibonacci(number);
+	return 0;
+}
