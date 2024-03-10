1e6b88d174b2416e82a7a6b0c14f0c348a718c93
twoo0220 <twoo0220@gmail.com>
2024-03-10 14:00:40 +0900

    24.03.10 - 1085

---
+++ BOJ/C++/1085.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x(0), y(0), w(0), h(0);
+	std::cin >> x >> y >> w >> h;
+
+	int xTemp = (w - x) < x ? (w - x) : x;
+	int yTemp = (h - y) < y ? (h - y) : y;
+
+	std::cout << ((xTemp > yTemp) ? yTemp : xTemp);
+
+	return 0;
+}
