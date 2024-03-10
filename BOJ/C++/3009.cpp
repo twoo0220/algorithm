6d1a55ea15671b612ec3b4bf568cc043c203f72a
twoo0220 <twoo0220@gmail.com>
2024-03-11 01:20:25 +0900

    24.03.11 - 3009

---
+++ BOJ/C++/3009.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x(0), y(0);
+	std::cin >> x >> y;
+	
+	int x1(0), y1(0), x2(0), y2(0);
+	std::cin >> x1 >> y1 >> x2 >> y2;
+
+	if (x == x1) x = x2;
+	else if (x == x2) x = x1;
+	if (y == y1) y = y2;
+	else if (y == y2) y = y1;
+
+	std::cout << x << " " << y;
+
+	return 0;
+}
