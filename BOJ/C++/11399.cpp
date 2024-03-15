559b24b832e42fc974b1aa69fbe34b40f51ea3e1
twoo0220 <twoo0220@gmail.com>
2024-03-16 00:16:25 +0900

    24.03.16 - 11399

---
+++ BOJ/C++/11399.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int size(0);
+	std::cin >> size;
+	
+	int* arr = new int[size];
+	for (int i = 0; i < size; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		arr[i] = temp;
+	}
+
+	std::sort(arr, arr + size);
+
+	int result = arr[0];
+	for (int i = size - 1; i > 0; --i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			result += arr[j];
+		}
+	}
+	
+	std::cout << result;
+	delete[] arr;
+	arr = nullptr;
+
+	return 0;
+}
