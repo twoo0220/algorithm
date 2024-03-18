079032775a516f5ce02515a78a575ebd9ced8181
twoo0220 <twoo0220@gmail.com>
2024-03-19 00:33:03 +0900

    24.03.19 - 1181

---
+++ BOJ/C++/1181.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+bool cmp(std::string a, std::string b)
+{
+	if (a.length() == b.length())
+	{
+		return a < b;
+	}
+	else
+	{
+		return a.length() < b.length();
+	}
+}
+
+int main()
+{
+	int size(0);
+	std::cin >> size;
+	
+	std::vector<std::string> mVector;
+	mVector.reserve(size);
+	for (int i = 0; i < size; ++i)
+	{
+		std::string temp;
+		std::cin >> temp;
+		mVector.push_back(temp);
+	}
+
+	std::sort(mVector.begin(), mVector.end(), cmp);
+
+	std::cout << mVector[0] << "\n";
+	for (int i = 1; i < size; ++i)
+	{
+		if (mVector[i] != mVector[i - 1])
+		{
+			std::cout << mVector[i] << "\n";
+		}
+	}
+
+	return 0;
+}
