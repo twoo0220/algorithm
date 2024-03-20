f48f93479d00ff1763b7c8f537c9cd4303e7a7f4
twoo0220 <twoo0220@gmail.com>
2024-03-21 00:56:58 +0900

    24.03.21 - 11650

---
+++ BOJ/C++/11650.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	int size(0);
+	std::cin >> size;
+
+	std::vector<std::pair<int, int>> mVector;
+	mVector.reserve(size);
+
+	for (int i = 0; i < size; ++i)
+	{
+		int a(0), b(0);
+		std::cin >> a >> b;
+		mVector.push_back(std::make_pair(a, b));
+	}
+
+	std::sort(mVector.begin(), mVector.end());
+
+	for (const auto& iter : mVector)
+	{
+		std::cout << iter.first << " " << iter.second << "\n";
+	}
+
+	return 0;
+}
