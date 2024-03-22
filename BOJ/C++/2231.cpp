8599711945b349cfa91f6822676412e99dcbf120
twoo0220 <twoo0220@gmail.com>
2024-03-23 00:28:41 +0900

    24.03.23 - 2231

---
+++ BOJ/C++/2231.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	int number(0);
+	std::cin >> number;
+
+	bool check = false;
+	for (int i = 1; i < number; ++i)
+	{
+		int temp = i;
+		int share = i;
+
+		while (share > 0)
+		{
+			temp += share % 10;
+			share /= 10;
+		}
+
+		if (temp == number)
+		{
+			std::cout << i;
+			check = true;
+			break;
+		}
+	}
+
+	if (false == check)
+	{
+		std::cout << 0;
+	}
+
+	return 0;
+}
