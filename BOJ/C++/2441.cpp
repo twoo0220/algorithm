3a2c430c0f01958f590ffacc4acfa2263d691206
twoo0220 <twoo0220@gmail.com>
2024-03-27 01:21:16 +0900

    24.03.27 - 2441

---
+++ BOJ/C++/2441.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	int size(0);
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int j = 0;
+		for (; j < i; ++j)
+		{
+			std::cout << ' ';
+		}
+		for (; j < size; ++j)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+}
