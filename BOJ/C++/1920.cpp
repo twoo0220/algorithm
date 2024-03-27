ec7b4b6d763095714270a0eb1775e2dc30aae9a4
twoo0220 <twoo0220@gmail.com>
2024-03-28 00:28:12 +0900

    24.03.28

---
+++ BOJ/C++/1920.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+int main()
+{
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	std::ios::sync_with_stdio(false);
+	std::unordered_map<int, bool> arr;
+
+	int size(0);
+	std::cin >> size;
+	for (int i = 0; i < size; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		arr.insert(std::make_pair(temp, true));
+	}
+
+	std::cin >> size;
+	for (int i = 0; i < size; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		std::cout << arr[temp] << '\n';
+	}
+
+	return 0;
+}
---
+++ BOJ/C++/2920.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	int a(0), b(0);
+	std::cin >> a;
+
+	std::string result = "mixed";
+	for (int i = 0; i < 7; ++i)
+	{
+		std::cin >> b;
+		if ((b - a) == 1)
+		{
+			result = "ascending";
+		}
+		else if ((b - a) == -1)
+		{
+			result = "descending";
+		}
+		else
+		{
+			result = "mixed";
+			break;
+		}
+		a = b;
+	}
+
+	std::cout << result;
+	return 0;
+}
