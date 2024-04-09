c8878b0043489463e435eb263e36de5a54c55332
twoo0220 <twoo0220@gmail.com>
2024-04-10 01:38:45 +0900

    24.04.10

---
+++ BOJ/C++/10814.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+bool cmp(std::pair<uint32_t, std::string> a, std::pair<uint32_t, std::string> b)
+{
+	return a.first < b.first ? true : false;
+}
+
+int main()
+{
+	uint32_t size = 0;
+	std::cin >> size;
+
+	std::vector<std::pair<uint32_t, std::string>> vec;
+	vec.reserve(size);
+
+	for (uint32_t i = 0; i < size; ++i)
+	{
+		uint32_t age = 0;
+		std::string name;
+		std::cin >> age >> name;
+		
+		vec.push_back(std::make_pair(age, name));
+	}
+
+	std::stable_sort(vec.begin(), vec.end(), cmp);
+
+	for (uint32_t i = 0; i < size; ++i)
+	{
+		std::cout << vec[i].first << " " << vec[i].second << "\n";
+	}
+
+	return 0;
+}
