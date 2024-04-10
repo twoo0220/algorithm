9732eda63183e6f70bb092fd4d581e294b0e1e87
twoo0220 <twoo0220@gmail.com>
2024-04-11 00:44:03 +0900

    24.04.11

---
+++ BOJ/C++/1427.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<uint32_t> vec;
+	uint32_t temp = 0;
+	std::cin >> temp;
+	while (temp)
+	{
+		vec.push_back(temp % 10);
+		temp /= 10;
+	}
+
+	std::sort(vec.rbegin(), vec.rend());
+
+	for (uint32_t i = 0; i < vec.size(); ++i)
+	{
+		std::cout << vec[i];
+	}
+
+	return 0;
+}
