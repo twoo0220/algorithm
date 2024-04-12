d743db243b29d9cc037ef87b892a0cded80ffca8
twoo0220 <twoo0220@gmail.com>
2024-04-12 15:09:29 +0900

    24.04.12 - 4153

---
+++ BOJ/C++/4153.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	while (true)
+	{
+		uint32_t a(0), b(0), c(0);
+		std::cin >> a >> b >> c;
+
+		if (0 == a) break;
+
+		uint32_t max = 0;
+		uint32_t remain1(0), remain2(0);
+		if (a > b)
+		{
+			remain1 = b;
+			if (a > c)
+			{
+				max = a;
+				remain2 = c;
+			}
+			else
+			{
+				max = c;
+				remain2 = a;
+			}
+		}
+		else
+		{
+			remain1 = a;
+			if (b > c)
+			{
+				max = b;
+				remain2 = c;
+			}
+			else
+			{
+				max = c;
+				remain2 = b;
+			}
+		}
+
+		if ((max * max) == ((remain1 * remain1) + (remain2 * remain2)))
+		{
+			std::cout << "right" << "\n";
+		}
+		else
+		{
+			std::cout << "wrong" << "\n";
+		}
+	}
+
+	return 0;
+}
