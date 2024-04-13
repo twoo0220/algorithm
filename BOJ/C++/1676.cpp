5e896a4a35b0f70445b7633cc0fc00e26b112188
twoo0220 <twoo0220@gmail.com>
2024-04-14 03:22:55 +0900

    24.04.14

---
+++ BOJ/C++/1676.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	uint32_t size = 0;
+	std::cin >> size;
+
+	uint32_t result = (size / 5) + (size / 25) + (size / 125);
+	std::cout << result;
+
+	return 0;
+}
