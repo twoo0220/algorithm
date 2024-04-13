799156494821a2e49406863239fa08438ba998b3
twoo0220 <twoo0220@gmail.com>
2024-04-13 19:03:19 +0900

    24.04.13

---
+++ BOJ/C++/11721.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string temp;
+	std::cin >> temp;
+
+	uint32_t size = temp.size();
+	std::string result;
+	result += temp[0];
+	for (uint32_t i = 1; i < size; ++i)
+	{
+		if ((i % 10) == 0)
+		{
+			std::cout << result << "\n";
+			result = "";
+			result += temp[i];
+		}
+		else
+		{
+			result += temp[i];
+		}
+	}
+	std::cout << result;
+
+	return 0;
+}
