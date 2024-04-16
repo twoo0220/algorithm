4dcf802b7b9ad85aae5190a6fcf7480f090795a1
twoo0220 <twoo0220@gmail.com>
2024-04-17 01:06:54 +0900

    24.04.17

---
+++ BOJ/C++/1158.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <queue>
+
+int main()
+{
+	int size(0), index(0);
+	std::cin >> size >> index;
+	
+	std::queue<int> que;
+	for (int i = 1; i <= size; ++i)
+	{
+		que.push(i);
+	}
+
+	std::cout << "<";
+	while (que.size() > 1)
+	{
+		for (int i = 1; i < index; ++i)
+		{
+			que.push(que.front());
+			que.pop();
+		}
+		std::cout << que.front() << ", ";
+		que.pop();
+	}
+	std::cout << que.front() << ">";
+	return 0;
+}
