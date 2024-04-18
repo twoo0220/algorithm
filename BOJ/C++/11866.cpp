bf20955aba3e9a071a939b011f7cd52c5d1a7793
twoo0220 <twoo0220@gmail.com>
2024-04-19 00:42:11 +0900

    24.04.19

---
+++ BOJ/C++/11866.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+
+int main()
+{
+	// 1158과 중복 문제
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+	int size(0), index(0);
+	std::cin >> size >> index;
+
+	std::queue<int> que;
+	for (int i = 1; i <= size; ++i)
+	{
+		que.push(i);
+	}
+
+	std::cout << "<";
+	while (que.size() > 1)
+	{
+		for (int i = 1; i < index; ++i)
+		{
+			que.push(que.front());
+			que.pop();
+		}
+		std::cout << que.front() << ", ";
+		que.pop();
+	}
+	std::cout << que.front() << ">";
+	return 0;
+}
