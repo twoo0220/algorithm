71331fb8361dcf98dfd0a71c4e555874a9fbb0a7
twoo0220 <twoo0220@gmail.com>
2024-04-21 02:14:55 +0900

    24.04.21

---
+++ BOJ/C++/10808.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int arr[26]{};
+	std::string word;
+	std::cin >> word;
+
+	for (int i = 0; i < word.size(); ++i)
+	{
+		arr[word[i] - 97]++;
+	}
+
+	for (int i = 0; i < 26; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
