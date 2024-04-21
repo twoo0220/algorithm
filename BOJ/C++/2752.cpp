27ff34cc90b5c73a68b5990fc4b745b6b202809d
twoo0220 <twoo0220@gmail.com>
2024-04-22 00:45:54 +0900

    24.04.22

---
+++ BOJ/C++/2752.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::vector<int> arr;
+	for (int i = 0; i < 3; ++i)
+	{
+		int temp(0);
+		std::cin >> temp;
+		arr.push_back(temp);
+	}
+
+	std::sort(arr.begin(), arr.end());
+
+	for (int i = 0; i < 3; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}
