db0627a0a3687de83f06fe8789e569cd9576ef19
twoo0220 <twoo0220@gmail.com>
2024-04-23 01:40:27 +0900

    24.04.23

---
+++ BOJ/C++/24265.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long number = 0;
+	std::cin >> number;
+	std::cout << ((number - 1) * number / 2) << "\n" << 2;
+
+	return 0;
+}
