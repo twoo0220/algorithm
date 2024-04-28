e4cf7a3ab2f9e2c9f565b2b93d1ce87c9bcf657b
크썸 <twoo0220@gmail.com>
2024-04-29 05:18:52 +0900

    24.04.29 mobile commit

---
+++ BOJ/C++/2163.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int a(0), b(0);
+    std::cin >> a >> b;
+
+    std::cout << ((b - 1) + (b * (a - 1)));
+    return 0;
+}
