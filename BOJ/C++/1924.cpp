2688d88a031a1e7bf49ca23d0d4b4f04f8cb38e4
twoo0220 <twoo0220@gmail.com>
2024-05-04 19:54:55 +0900

    24.05.04

---
+++ BOJ/C++/1924.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::string day[7] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+	int month[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	int mm(0), dd(0);
+	std::cin >> mm >> dd;
+
+	for (int i = 0; i < mm; ++i)
+	{
+		dd += month[i];
+	}
+
+	std::cout << day[dd % 7];
+	return 0;
+}
