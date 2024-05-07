c0ca316ab01e8d8d4accaa1f66e1be7161fe3257
twoo0220 <twoo0220@gmail.com>
2024-05-07 19:16:20 +0900

    24.05.07

---
+++ BOJ/C++/11050.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+static int binomial(int a, int b)
+{
+	if ((b == 0) || (a == b))
+	{
+		return 1;
+	}
+
+	return binomial(a - 1, b - 1) + binomial(a - 1, b);
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0);
+	std::cin >> a >> b;
+
+	std::cout << binomial(a, b);
+
+	return 0;
+}
