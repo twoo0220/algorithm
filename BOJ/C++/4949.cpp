df989f063e224c1aea8bb2574661549c6b7c52c2
twoo0220 <twoo0220@gmail.com>
2024-05-08 00:59:00 +0900

    24.05.08

---
+++ BOJ/C++/4949.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		std::string line;
+		std::getline(std::cin, line);
+
+		int size = static_cast<int>(line.length());
+		if ((size == 1) && line[0] == '.')
+		{
+			break;
+		}
+
+		std::stack<char> st;
+		bool pairCheck = true;
+		for (int i = 0; i < size; ++i)
+		{
+			if (line[i] == '(')
+			{
+				st.push(line[i]);
+			}
+			else if (line[i] == '[')
+			{
+				st.push(line[i]);
+			}
+			else if (line[i] == ')')
+			{
+				if ((!st.empty()) && (st.top() == '('))
+				{
+					st.pop();
+				}
+				else
+				{
+					goto result;
+				}
+			}
+			else if (line[i] == ']')
+			{
+				if ((!st.empty()) && (st.top() == '['))
+				{
+					st.pop();
+				}
+				else
+				{
+					goto result;
+				}
+			}
+		}
+
+		if (st.empty())
+		{
+			std::cout << "yes\n";
+		}
+		else
+		{
+result:
+			std::cout << "no\n";
+		}
+	}
+
+	return 0;
+}
