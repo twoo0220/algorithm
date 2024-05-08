7f63969d6fc76dd3eeec0e7f3e2b71987ec17732
twoo0220 <twoo0220@gmail.com>
2024-05-09 00:33:55 +0900

    24.05.09

---
+++ BOJ/C++/11729.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+void hanoi(int n, int from, int by, int to)
+{
+	if (n == 0) return;
+	hanoi(n - 1, from, to, by);
+	std::cout << from << " " << to << "\n";
+	hanoi(n - 1, by, from, to);
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+	int count = std::pow(2, size) - 1;
+	std::cout << count << "\n";
+
+	hanoi(size, 1, 2, 3);
+	return 0;
+}
