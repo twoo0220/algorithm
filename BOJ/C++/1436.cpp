f60599f5e41e7f0592981746ea09f7b3ab494b57
twoo0220 <twoo0220@gmail.com>
2024-05-12 21:22:11 +0900

    24.05.12

---
+++ BOJ/C++/1436.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	int result = 0;
+
+	while (count > 0)
+	{
+		result++;
+		int temp = result;
+		while (temp != 0)
+		{
+			if ((temp % 1000) == 666)
+			{
+				count--;
+				break;
+			}
+			else
+			{
+				temp /= 10;
+			}
+		}
+	}
+
+	std::cout << result;
+	return 0;
+}
