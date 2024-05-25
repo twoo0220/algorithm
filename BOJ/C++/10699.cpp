0c30f99dd39a009fd6b1f837f4f15bfe43ecb839
twoo0220 <twoo0220@gmail.com>
2024-05-25 18:00:14 +0900

    24.05.25

---
+++ BOJ/C++/10699.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    time_t timer = time(nullptr);
+    //tm time{};
+    //localtime_s(&time, &timer);
+
+    //std::cout << time.tm_year + 1900 << "-"
+    //    << time.tm_mon + 1 << "-"
+    //    << time.tm_mday;
+    tm* time = localtime(&timer);
+    std::cout << time->tm_year + 1900 << "-";
+
+    std::cout.width(2);
+    std::cout.fill('0');
+    std::cout << time->tm_mon + 1 << "-";
+
+    std::cout.width(2);
+    std::cout.fill('0');
+    std::cout << time->tm_mday;
+
+    return 0;
+}
