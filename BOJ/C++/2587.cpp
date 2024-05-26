b0eacb90f5f69182326471d324c5b844e517d949
twoo0220 <twoo0220@gmail.com>
2024-05-26 19:03:31 +0900

    24.05.26

---
+++ BOJ/C++/2587.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    std::vector<int> arr(5);
+    int total = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cin >> arr[i];
+        total += arr[i];
+    }
+
+    std::sort(arr.begin(), arr.end());
+    std::cout << (total / 5) << "\n";
+    std::cout << arr[2];
+
+    return 0;
+}
