60b9ae949ab8201e3a339b261aeec8f3029c9e02
twoo0220 <twoo0220@gmail.com>
2024-05-29 00:13:33 +0900

    24.05.29

---
+++ BOJ/C++/2490.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        int total = 0;
+        for (int j = 0; j < 4; ++j)
+        {
+            int count = 0;
+            std::cin >> count;
+            total += count;
+        }
+
+        if (total == 0)
+        {
+            std::cout << "D\n";
+        }
+        else if (total == 1)
+        {
+            std::cout << "C\n";
+        }
+        else if (total == 2)
+        {
+            std::cout << "B\n";
+        }
+        else if (total == 3)
+        {
+            std::cout << "A\n";
+        }
+        else
+        {
+            std::cout << "E\n";
+        }
+    }
+    return 0;
+}
