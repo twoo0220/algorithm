650c6ac3cfb608454e646c7493a1c7b1c8333e6f
twoo0220 <twoo0220@gmail.com>
2024-06-07 00:27:40 +0900

    24.06.07

---
+++ BOJ/C++/5086.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    while (true)
+    {
+        int a(0), b(0);
+        std::cin >> a >> b;
+
+        if (a == 0)
+        {
+            break;
+        }
+        
+        if ((a % b) == 0)
+        {
+            std::cout << "multiple\n";
+        }
+        else if ((b % a) == 0)
+        {
+            std::cout << "factor\n";
+        }
+        else
+        {
+            std::cout << "neither\n";
+        }
+    }
+
+    return 0;
+}
