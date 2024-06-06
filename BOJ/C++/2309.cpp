8ccf3bf7401a60e83c169bb95ce3de2e1d2db426
twoo0220 <twoo0220@gmail.com>
2024-06-07 00:35:23 +0900

    24.06.07

---
+++ BOJ/C++/2309.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int arr[9]{};
+    int sum = 0;
+    for (int i = 0; i < 9; ++i)
+    {
+        std::cin >> arr[i];
+        sum += arr[i];
+    }
+
+    std::sort(arr, arr + 9);
+    for (int i = 0; i < 8; ++i)
+    {
+        for (int j = i + 1; j < 9; ++j)
+        {
+            if ((sum - arr[i] - arr[j]) == 100)
+            {
+                for (int k = 0; k < 9; ++k)
+                {
+                    if ((k != i) && (k != j))
+                    {
+                        std::cout << arr[k] << "\n";
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
