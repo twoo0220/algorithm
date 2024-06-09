8602fe83da74f915b9a142942a1b6220bb30badb
twoo0220 <twoo0220@gmail.com>
2024-06-10 01:29:33 +0900

    24.06.10

---
+++ BOJ/C++/2775.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int arr[15][15]{};
+    for (int i = 0; i < 15; ++i)
+    {
+        arr[0][i] = i;
+    }
+
+    for (int i = 1; i < 15; ++i)
+    {
+        for (int j = 0; j < 15; ++j)
+        {
+            for (int k = 0; k <= j; ++k)
+            {
+                arr[i][j] += arr[i - 1][k];
+            }
+        }
+    }
+
+    int count = 0;
+    std::cin >> count;
+
+    for (int i = 0; i < count; ++i)
+    {
+        int k(0), n(0);
+        std::cin >> k >> n;
+        std::cout << arr[k][n] << "\n";
+    }
+
+    return 0;
+}
