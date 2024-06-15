563747e4a139fe5b8145ff650f502d4535d4c8cd
twoo0220 <twoo0220@gmail.com>
2024-06-15 21:46:58 +0900

    24.06.15

---
+++ BOJ/C++/3046.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int R1(0), S(0);
+	std::cin >> R1 >> S;
+	std::cout << (2 * S - R1);
+	
+	return 0;
+}
