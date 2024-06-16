13991e2f7decae4af8d6ce2453b7360eb3e47924
twoo0220 <twoo0220@gmail.com>
2024-06-17 00:43:14 +0900

    24.06.17

---
+++ BOJ/C++/11653.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+// https://kbhetrr.dev/blog/prime-factorization/ 참고
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> number;
+
+	for (int i = 2; i * i <= number; ++i)
+	{
+		while (number % i == 0)
+		{
+			std::cout << i << "\n";
+			number /= i;
+		}
+	}
+
+	if (number > 1)
+	{
+		std::cout << number;
+	}
+
+	return 0;
+}
