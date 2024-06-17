69928eb457b1e68d7bab6634816665b6b122df44
twoo0220 <twoo0220@gmail.com>
2024-06-18 01:06:56 +0900

    24.06.18

---
+++ BOJ/C++/3053.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	const double pi = 3.14159265358979323846;
+	double radius = 0;
+	std::cin >> radius;
+
+	printf("%lf\n", (pi * radius * radius));
+	printf("%lf", (2.0f * radius * radius));
+
+	return 0;
+}
