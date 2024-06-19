fd12da747e2f813eba5422acbfa465b3a49adaed
twoo0220 <twoo0220@gmail.com>
2024-06-20 00:47:05 +0900

    24.06.20

---
+++ BOJ/C++/2609.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int gcd(int a, int b)
+{
+	int c = 0;
+	while (b)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0);
+	std::cin >> a >> b;
+
+	int max = gcd(a, b);
+	std::cout << max << "\n";
+	std::cout << ((a * b) / max);
+
+	return 0;
+}
