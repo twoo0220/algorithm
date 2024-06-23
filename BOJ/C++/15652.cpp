f8bf6de2c8534c6b99b7924a1a3f4238ffa65f91
twoo0220 <twoo0220@gmail.com>
2024-06-24 00:37:55 +0900

    24.06.24

---
+++ BOJ/C++/15652.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+int N(0), M(0);
+bool visited[9]{};
+int number[9]{};
+
+void dfs(int cnt, int size)
+{
+	if (size == M)
+	{
+		for (int i = 0; i < M; ++i)
+		{
+			std::cout << number[i] << ' ';
+		}
+		std::cout << "\n";
+	}
+	else
+	{
+		for (int i = cnt; i <= N; ++i)
+		{
+			//if (visited[i] == false)
+			//{
+			//	visited[i] = true;
+				number[size] = i;
+				dfs(i, size + 1);
+			//	visited[i] = false;
+			//}
+		}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::cin >> N >> M;
+	dfs(1, 0);
+	return 0;
+}
