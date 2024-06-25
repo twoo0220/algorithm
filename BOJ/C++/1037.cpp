558900f13007479243c2e411127c5dae3a865c02
twoo0220 <twoo0220@gmail.com>
2024-06-26 00:05:46 +0900

    24.06.26

---
+++ BOJ/C++/1037.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	int min(1000001), max(0);
+	for (int i = 0; i < count; ++i)
+	{
+		int number = 0;
+		std::cin >> number;
+		
+		if (number < min)
+		{
+			min = number;
+		}
+
+		if (number > max)
+		{
+			max = number;
+		}
+	}
+
+	std::cout << static_cast<long long>(min) * static_cast<long long>(max);
+	return 0;
+}
