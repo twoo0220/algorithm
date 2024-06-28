a69c840def7e9ed13cfda0da606b7ce343c4d731
twoo0220 <twoo0220@gmail.com>
2024-06-28 23:22:58 +0900

    24.06.28

---
+++ BOJ/C++/2446.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size - 1; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << " ";
+		}
+
+		int length = (size - i) * 2 - 1;
+		for (int j = 0; j < length; ++j)
+		{
+			std::cout << "*";
+		}
+
+		std::cout << "\n";
+	}
+
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size - i - 1; ++j)
+		{
+			std::cout << " ";
+		}
+
+		for (int j = 0; j < 2 * i + 1; ++j)
+		{
+			std::cout << "*";
+		}
+
+		std::cout << "\n";
+	}
+
+	return 0;
+}
