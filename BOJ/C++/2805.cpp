b2b9704809982612e47391a08310a5e1d2470428
twoo0220 <twoo0220@gmail.com>
2024-07-01 00:05:11 +0900

    24.07.01

---
+++ BOJ/C++/2805.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	long long length = 0;
+	std::cin >> count >> length;
+
+	long long* tree = new long long[count] {};
+
+	for (int i = 0; i < count; ++i)
+	{
+		std::cin >> tree[i];
+	}
+
+	std::sort(tree, tree + count);
+	long long min(0), result(0);
+	long long max = tree[count - 1];
+
+	while (min <= max)
+	{
+		long long sum = 0;
+		long long mid = (min + max) / 2;
+
+		for (int i = 0; i < count; ++i)
+		{
+			if ((tree[i] - mid) > 0)
+			{
+				sum += tree[i] - mid;
+			}
+		}
+
+		if (sum >= length)
+		{
+			min = mid + 1;
+			result = mid;
+		}
+		else
+		{
+			max = mid - 1;
+		}
+	}
+
+	std::cout << result;
+
+	delete[] tree;
+	tree = nullptr;
+
+	return 0;
+}
