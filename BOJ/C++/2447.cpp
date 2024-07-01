1f189514426b7dabcd905b4e54d044d41098c654
twoo0220 <twoo0220@gmail.com>
2024-07-02 00:08:02 +0900

    24.07.02

---
+++ BOJ/C++/2447.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+void pattern(int row, int column, int size)
+{
+	if ((((row / size) % 3) == 1) && (((column / size) % 3) == 1))
+	{
+		std::cout << ' ';
+	}
+	else
+	{
+		if ((size / 3) == 0)
+		{
+			std::cout << '*';
+		}
+		else
+		{
+			pattern(row, column, (size / 3));
+		}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+		{
+			pattern(i, j, size);
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+}
