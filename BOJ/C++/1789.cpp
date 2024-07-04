e090811226ea6ad140024b2c10fab8f0b5a9ec77
twoo0220 <twoo0220@gmail.com>
2024-07-05 01:45:12 +0900

    24.07.05

---
+++ BOJ/C++/1789.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long number = 0;
+	std::cin >> number;
+
+	long long result = 0;
+	while (number > 0)
+	{
+		result++;
+		number -= result;
+	}
+
+	if (number < 0)
+	{
+		result--;
+	}
+
+	std::cout << result;
+	return 0;
+}
