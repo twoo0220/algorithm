3b59dd387686045790536f8e536e1513d966c8db
twoo0220 <twoo0220@gmail.com>
2024-07-06 02:44:18 +0900

    24.07.06

---
+++ BOJ/C++/6603.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+int count = 0;
+int lotto[13]{};
+int arr[6]{};
+
+void dfs(int start, int depth)
+{
+	if (depth == 6)
+	{
+		for (int i = 0; i < 6; ++i)
+		{
+			std::cout << arr[i] << ' ';
+		}
+		std::cout << "\n";
+	}
+	else
+	{
+		for (int i = start; i < count; ++i)
+		{
+			arr[depth] = lotto[i];
+			dfs(i + 1, depth + 1);
+		}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		count = 0;
+		std::cin >> count;
+
+		if (count == 0)
+		{
+			break;
+		}
+
+		for (int j = 0; j < count; ++j)
+		{
+			std::cin >> lotto[j];
+		}
+
+		dfs(0, 0);
+		std::cout << "\n";
+	}
+
+	return 0;
+}
