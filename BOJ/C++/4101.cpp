b533777c04518a3e28f8f0732c95b0b56995fae3
twoo0220 <twoo0220@gmail.com>
2024-07-09 01:03:36 +0900

    24.07.09

---
+++ BOJ/C++/4101.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		int a(0), b(0);
+		std::cin >> a >> b;
+
+		if ((a == 0) && (b == 0))
+		{
+			break;
+		}
+		else if (a > b)
+		{
+			std::cout << "Yes\n";
+		}
+		else
+		{
+			std::cout << "No\n";
+		}
+	}
+
+	return 0;
+}
