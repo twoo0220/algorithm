020610f862dfc5185756a0576c1d2b097eb21d35
twoo0220 <twoo0220@gmail.com>
2024-07-14 16:32:08 +0900

    24.07.14 - string format

---
+++ BOJ/C++/5338.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::cout << "       _.-;;-._\n";
+	std::cout << "'-..-'|   ||   |\n";
+	std::cout << "'-..-'|_.-;;-._|\n";
+	std::cout << "'-..-'|   ||   |\n";
+	std::cout << "'-..-'|_.-''-._|\n";
+
+	return 0;
+}
