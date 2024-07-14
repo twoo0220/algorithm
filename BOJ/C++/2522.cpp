41153335054a2b72ffff71d0d86430a6258a3b69
twoo0220 <twoo0220@gmail.com>
2024-07-15 00:09:21 +0900

    24.07.15 - string format

---
+++ BOJ/C++/2522.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 1; i <= size; ++i)
+	{
+		for (int j = 0; j < size - i; ++j)
+		{
+			std::cout << " ";
+		}
+
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+
+	for (int i = 1; i <= size; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << " ";
+		}
+
+		for (int j = 0; j < size - i; ++j)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
