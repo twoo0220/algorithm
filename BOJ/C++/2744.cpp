1e4bc6d432c62c7b2004342d852f50368d3c6ca2
twoo0220 <twoo0220@gmail.com>
2024-07-16 00:26:46 +0900

    24.07.16 - string,ascii

---
+++ BOJ/C++/2744.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::string temp;
+	std::cin >> temp;
+
+	for (int i = 0; i < temp.size(); ++i)
+	{
+		if (temp[i] < 97)
+		{
+			temp[i] += 32;
+		}
+		else
+		{
+			temp[i] -= 32;
+		}
+	}
+
+	std::cout << temp;
+
+	return 0;
+}
