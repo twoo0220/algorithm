6157127778b0baafe631bb599d013af1efad4be3
twoo0220 <twoo0220@gmail.com>
2024-07-24 00:02:03 +0900

    24.07.24 - math, equation

---
+++ BOJ/C++/19532.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0), d(0), e(0), f(0);
+	std::cin >> a >> b >> c >> d >> e >> f;
+
+	std::cout << (((b * f) - (c * e)) / ((b * d) - (a * e))) << " ";
+	std::cout << (((a * f) - (c * d)) / ((a * e) - (b * d)));
+
+	return 0;
+}
