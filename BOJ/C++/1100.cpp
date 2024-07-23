90911a016d846ea2978902bbf7b314bcb35885f2
twoo0220 <twoo0220@gmail.com>
2024-07-23 22:41:10 +0900

    24.07.23 - string, char, math

---
+++ BOJ/C++/1100.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int odd = 1;
+	int result = 0;
+	for (int i = 1; i < 9; ++i)
+	{
+		for (int j = 1; j < 9; ++j)
+		{
+			char temp{};
+			std::cin >> temp;
+
+			if ((temp == 'F') && (j % 2 == odd))
+			{
+				result++;
+			}
+
+		}
+		odd = (i + 1) % 2;
+	}
+
+	std::cout << result;
+	return 0;
+}
