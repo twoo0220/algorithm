2287aee627532fe9d44dee4b9ce9790373f64ff8
twoo0220 <twoo0220@gmail.com>
2024-07-26 01:37:33 +0900

    24.07.26 - arithmetic, math

---
+++ BOJ/C++/10156.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int price(0), count(0), money(0);
+	std::cin >> price >> count >> money;
+
+	price *= count;
+	int deficit = price - money;
+
+	if (deficit > 0)
+	{
+		std::cout << deficit;
+	}
+	else
+	{
+		std::cout << 0;
+	}
+
+	return 0;
+}
