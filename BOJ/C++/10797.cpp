a3130bb0dbe7f22f878dacf5e5f9e09af27d5621
twoo0220 <twoo0220@gmail.com>
2024-07-26 01:42:45 +0900

    24.07.26 - arithmetic, math

---
+++ BOJ/C++/10797.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> number;
+
+	int count = 0;
+	for (int i = 0; i < 5; ++i)
+	{
+		int car = 0;
+		std::cin >> car;
+		if (car == number)
+		{
+			count++;
+		}
+	}
+	std::cout << count;
+
+	return 0;
+}
