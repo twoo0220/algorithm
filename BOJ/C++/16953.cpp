a5f0924f104cc79477f8f8d174d12cda22f533f0
twoo0220 <twoo0220@gmail.com>
2024-07-27 18:39:39 +0900

    24.07.27 - math

---
+++ BOJ/C++/16953.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0);
+	std::cin >> a >> b;
+
+	int count = 1;
+	while (true)
+	{
+		if (a == b)
+		{
+			break;
+		}
+
+		if (a > b)
+		{
+			count = -1;
+			break;
+		}
+
+		if (b % 2 == 0)
+		{
+			b /= 2;
+		}
+		else if (b % 10 == 1)
+		{
+			b--;
+			b /= 10;
+		}
+		else
+		{
+			count = -1;
+			break;
+		}
+
+		count++;
+	}
+
+	std::cout << count;
+	return 0;
+}
