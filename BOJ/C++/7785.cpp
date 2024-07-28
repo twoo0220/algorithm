8a4af3eb750f895090501317a2e671d64cfe4072
twoo0220 <twoo0220@gmail.com>
2024-07-29 02:12:54 +0900

    24.07.29 - hashmap

---
+++ BOJ/C++/7785.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	std::map < std::string, int, std::greater<std::string>> workMap;
+	for (int i = 0; i < count; ++i)
+	{
+		std::string name, status;
+		std::cin >> name >> status;
+
+		if (workMap.find(name) == workMap.end())
+		{
+			workMap.insert({ name, 0 });
+		}
+		else
+		{
+			workMap.erase(name);
+		}
+	}
+
+	for (auto& iter : workMap)
+	{
+		std::cout << iter.first << "\n";
+	}
+
+	return 0;
+}
