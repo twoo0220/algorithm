3ab16d056ce545d37dbc6f758d17975baa4d3664
twoo0220 <twoo0220@gmail.com>
2024-07-30 02:01:11 +0900

    24.07.30 - math, string, parsing

---
+++ BOJ/C++/1541.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::string equation;
+	std::cin >> equation;
+
+	size_t size = equation.size();
+	int result = 0;
+	std::string temp;
+	bool bCheck = false;
+	for (size_t i = 0; i < size; ++i)
+	{
+		if ((equation[i] == '+') || equation[i] == '-')
+		{
+			if (bCheck)
+			{
+				result -= std::stoi(temp);
+			}
+			else
+			{
+				result += std::stoi(temp);
+			}
+			temp.clear();
+		}
+		else
+		{
+			temp += equation[i];
+		}
+
+		if (equation[i] == '-')
+		{
+			bCheck = true;
+		}
+	}
+
+	if (bCheck)
+	{
+		result -= std::stoi(temp);
+	}
+	else
+	{
+		result += std::stoi(temp);
+	}
+
+	std::cout << result;
+	return 0;
+}
