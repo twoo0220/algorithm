9117ec9054e4244de86e6d5a0738cf8a02a3dca4
twoo0220 <twoo0220@gmail.com>
2024-08-04 01:02:32 +0900

    24.08.04 - string, star

---
+++ BOJ/C++/10991.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 1; i <= size; ++i)
+	{
+		for (int j = size - i; j > 0; --j)
+		{
+			std::cout << ' ';
+		}
+
+		for (int j = 1; j <= i; ++j)
+		{
+			std::cout << '*' << ' ';
+		}
+
+		std::cout << '\n';
+	}
+	return 0;
+}
