13b829a2e490578386c5512748b2d2c48b678ba3
twoo0220 <twoo0220@gmail.com>
2024-08-06 01:36:19 +0900

    24.08.06 - math, geometry

---
+++ BOJ/C++/9063.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int maxX(-10001), minX(10001), maxY(-10001), minY(10001), count(0);
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int x(0), y(0);
+		std::cin >> x >> y;
+
+		if (x >= maxX)
+		{
+			maxX = x;
+		}
+
+		if (minX >= x)
+		{
+			minX = x;
+		}
+
+		if (y >= maxY)
+		{
+			maxY = y;
+		}
+
+		if (minY >= y)
+		{
+			minY = y;
+		}
+	}
+
+	std::cout << (maxX - minX) * (maxY - minY);
+	return 0;
+}
