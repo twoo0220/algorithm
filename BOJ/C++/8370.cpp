7f4219a9baa8505bd7efd51861afd22398ddad61
twoo0220 <twoo0220@gmail.com>
2024-08-06 01:39:34 +0900

    24.08.06 - arithmetic

---
+++ BOJ/C++/8370.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int n1(0), k1(0), n2(0), k2(0);
+	std::cin >> n1 >> k1 >> n2 >> k2;
+	std::cout << (n1 * k1) + (n2 * k2);
+	return 0;
+}
