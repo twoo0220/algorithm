28e00afb962a63a808b546cbeae946a74ea62f00
twoo0220 <twoo0220@gmail.com>
2024-08-07 00:18:13 +0900

    24.08.07 - string

---
+++ BOJ/C++/25630.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	std::string food;
+	std::cin >> food;
+	
+	int count = 0;
+	for (int i = 0; i < size / 2; ++i)
+	{
+		if (food[i] != food[size - i - 1])
+		{
+			count++;
+		}
+	}
+	std::cout << count;
+
+	return 0;
+}
