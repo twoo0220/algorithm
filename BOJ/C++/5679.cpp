5fe6bd98a6a01a6303033d66000c0783761948c8
twoo0220 <twoo0220@gmail.com>
2024-08-09 01:24:34 +0900

    24.08.09 - math

---
+++ BOJ/C++/5679.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		int number = 0;
+		std::cin >> number;
+
+		if (number == 0)
+		{
+			break;
+		}
+
+		int max = number;
+		
+		while (number != 1)
+		{
+			if (number % 2 == 0)
+			{
+				number = number / 2;
+			}
+			else
+			{
+				number = 3 * number + 1;
+			}
+			
+			if (number > max)
+			{
+				max = number;
+			}
+		}
+		std::cout << max << '\n';
+	}
+
+	return 0;
+}
