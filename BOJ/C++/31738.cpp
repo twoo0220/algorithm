c4163135f668dfe2dae197634007ee8d12f880ca
twoo0220 <twoo0220@gmail.com>
2024-08-10 00:23:03 +0900

    24.08.10 - math, Number Theory, remainder theorem

---
+++ BOJ/C++/31738.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long a(0), b(0), result(0);
+	std::cin >> a >> b;
+
+	if (a < b)
+	{
+		result = 1;
+		for (long long i = a; i > 0; --i)
+		{
+			result *= i;
+			result %= b;
+		}
+		result %= b;
+	}
+	std::cout << result;
+
+	return 0;
+}
