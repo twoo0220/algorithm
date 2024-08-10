d147030281ba25754d9de76b8bf1943c42c217c6
twoo0220 <twoo0220@gmail.com>
2024-08-11 02:58:18 +0900

    24.08.11 - string

---
+++ BOJ/C++/30402.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	char temp = 0;
+	for (int i = 0; i < 15; ++i)
+	{
+		for (int j = 0; j < 15; ++j)
+		{
+			std::cin >> temp;
+			
+			if (temp == 'w')
+			{
+				std::cout << "chunbae";
+				goto finish;
+			}
+			else if (temp == 'b')
+			{
+				std::cout << "nabi";
+				goto finish;
+			}
+			else if (temp == 'g')
+			{
+				std::cout << "yeongcheol";
+				goto finish;
+			}
+		}
+	}
+finish:
+	return 0;
+}
