b92443161b28cbbf74cba4ac28392c8a9b46b587
twoo0220 <twoo0220@gmail.com>
2024-08-12 00:46:23 +0900

    24.08.12 - number, math

---
+++ BOJ/C++/1475.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count[9]{};
+	int number(0), max(0);
+	std::cin >> number;
+
+	while (number > 0)
+	{
+		int temp = number % 10;
+		number /= 10;
+
+		if (temp == 9)
+		{
+			count[6]++;
+		}
+		else
+		{
+			count[temp]++;
+		}
+	}
+
+	count[6] = (count[6] + 1) / 2;
+	max = count[0];
+	for (int i = 1; i < 9; ++i)
+	{
+		if (max < count[i])
+		{
+			max = count[i];
+		}
+	}
+	std::cout << max;
+
+	return 0;
+}
