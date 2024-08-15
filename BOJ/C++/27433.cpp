a45450ed4e0d037d1517587564bfe5f1183cdad9
twoo0220 <twoo0220@gmail.com>
2024-08-16 02:17:19 +0900

    24.08.16 - math, factorial

---
+++ BOJ/C++/27433.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+long long factorial[21]
+{
+	1,
+	1,
+	2,
+	6,
+	24,
+	120,
+	720,
+	5040,
+	40320,
+	362880,
+	3628800,
+	39916800,
+	479001600,
+	6227020800,
+	87178291200,
+	1307674368000,
+	20922789888000,
+	355687428096000,
+	6402373705728000,
+	121645100408832000,
+	2432902008176640000
+};
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int index = 0;
+	std::cin >> index;
+	std::cout << factorial[index];
+
+	// simple is best
+	return 0;
+}
