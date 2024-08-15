e445c7965da3a9f4878b88af422d69f95a69a37f
twoo0220 <twoo0220@gmail.com>
2024-08-15 22:22:21 +0900

    24.08.15 - math, brute force

---
+++ BOJ/C++/3276.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int row(1), column(1), number(0);
+	std::cin >> number;
+
+	while ((row * column) < number)
+	{
+		if (row > column)
+		{
+			column++;
+		}
+		else
+		{
+			row++;
+		}
+	}
+	std::cout << row << ' ' << column;
+
+	return 0;
+}
