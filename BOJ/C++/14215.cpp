8e79d36f8ea2ed54faf81ab29c95d79d873e4966
twoo0220 <twoo0220@gmail.com>
2024-08-17 03:19:07 +0900

    24.08.17 - math, geometry

---
+++ BOJ/C++/14215.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::vector<int> vec;
+	vec.reserve(3);
+	for (int i = 0; i < 3; ++i)
+	{
+		int number = 0;
+		std::cin >> number;
+		vec.push_back(number);
+	}
+
+	std::sort(vec.begin(), vec.end());
+
+	if ((vec[0] + vec[1]) > vec[2])
+	{
+		std::cout << (vec[0] + vec[1] + vec[2]);
+	}
+	else
+	{
+		std::cout << ((vec[0] + vec[1]) * 2 - 1);
+	}
+
+	return 0;
+}
