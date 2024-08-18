4ed1a80f4b343df67e2548c8aa7cd472adf6e680
twoo0220 <twoo0220@gmail.com>
2024-08-19 00:01:51 +0900

    24.08.19 - greedy algorithm, stack

---
+++ BOJ/C++/2812.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::vector<char> vec;
+	std::string number;
+	int size(0), out(0);
+	std::cin >> size >> out;
+	std::cin >> number;
+
+	for (int i = 0; i < size; ++i)
+	{
+		while ((out > 0) && (!vec.empty()) && (number[i] > vec.back()))
+		{
+			out--;
+			vec.pop_back();
+		}
+		vec.push_back(number[i]);
+	}
+
+	while (out--)
+	{
+		vec.pop_back();
+	}
+
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		std::cout << vec[i];
+	}
+
+	return 0;
+}
