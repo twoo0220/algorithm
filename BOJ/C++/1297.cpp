12bcee7a31c43a828423729ec37ff5d4b61323e5
twoo0220 <twoo0220@gmail.com>
2024-08-20 01:34:48 +0900

    24.08.20 - math, data type, pythagoras theorem

---
+++ BOJ/C++/1297.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	double length(0), h(0), w(0);
+	std::cin >> length >> h >> w;
+
+	double height = std::sqrt(((length * length) * (h * h)) / ((h * h) + (w * w)));
+	double width = height * w / h;
+
+	std::cout << static_cast<int>(height) << ' ' << static_cast<int>(width);
+
+	return 0;
+}
