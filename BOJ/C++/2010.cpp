5d861f41a2b6590f638b1a2e7f46e4b0c7ade04d
twoo0220 <twoo0220@gmail.com>
2024-08-22 00:01:00 +0900

    24.08.22 - arithmetic

---
+++ BOJ/C++/2010.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count(0), result(1);
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int plug = 0;
+		std::cin >> plug;
+		result += plug;
+	}
+
+	std::cout << (result - count);
+
+	return 0;
+}
