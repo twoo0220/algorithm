810af80edd535b9e2853f5ef36dde950d8b2ecf5
twoo0220 <twoo0220@gmail.com>
2024-08-23 01:13:31 +0900

    24.08.23 - arithmetic

---
+++ BOJ/C++/2953.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int index(0), max(0);
+	for (int i = 0; i < 5; ++i)
+	{
+		int result = 0;
+		for (int j = 0; j < 4; ++j)
+		{
+			int score = 0;
+			std::cin >> score;
+			result += score;
+		}
+
+		if (result > max)
+		{
+			index = i;
+			max = result;
+		}
+	}
+
+	std::cout << (index + 1) << ' ' << max;
+
+	return 0;
+}
