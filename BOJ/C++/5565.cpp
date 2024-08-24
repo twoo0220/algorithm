85702681b6700fc020d6f7ee0b4a6b19d8c2441c
twoo0220 <twoo0220@gmail.com>
2024-08-25 00:15:39 +0900

    24.08.25 - arithmetic

---
+++ BOJ/C++/5565.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int total(0), sum(0);
+	std::cin >> total;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		int temp = 0;
+		std::cin >> temp;
+		sum += temp;
+	}
+
+	std::cout << (total - sum);
+
+	return 0;
+}
