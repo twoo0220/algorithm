5d017a95ba7315968c2de2743192a699af8f4330
twoo0220 <twoo0220@gmail.com>
2024-08-26 01:47:09 +0900

    24.08.26 - dynamic programming, fibonacci

---
+++ BOJ/C++/24416.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int fibo[100]{};
+	fibo[1] = 1;
+	fibo[2] = 1;
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 3; i <= count; ++i)
+	{
+		fibo[i] = fibo[i - 2] + fibo[i - 1];
+	}
+
+	std::cout << fibo[count] << ' ' << count - 2;
+	return 0;
+}
