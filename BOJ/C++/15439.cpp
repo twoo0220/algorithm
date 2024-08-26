842e1bf634112ea11e7c4262a4f63b2686cbfe9d
twoo0220 <twoo0220@gmail.com>
2024-08-27 01:59:24 +0900

    24.08.27 - combinatorics

---
+++ BOJ/C++/15439.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int N = 0;
+	std::cin >> N;
+	std::cout << N * (N - 1);
+	return 0;
+}
