4ebc7f1331335515ae4da4c433f1022b2b7915f8
twoo0220 <twoo0220@gmail.com>
2024-08-27 02:01:52 +0900

    24.08.27 - arithmetic

---
+++ BOJ/C++/16394.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int year = 0;
+	std::cin >> year;
+	std::cout << (year - 1946);
+	return 0;
+}
