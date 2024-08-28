d3811c1ae63a7a79ee7a1e942aeec893a50d2bb9
twoo0220 <twoo0220@gmail.com>
2024-08-29 00:21:18 +0900

    24.08.29 - gcd(Greatest Common Divisor), lcm(least common multiple)

---
+++ BOJ/C++/13241.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+long long int gcd(long long int a, long long int b)
+{
+	long long int c = 0;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long int a(0), b(0);
+	std::cin >> a >> b;
+	std::cout << ((a * b) / gcd(a, b));
+
+	return 0;
+}
