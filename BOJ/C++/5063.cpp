b0f118c1739ed30377a141bb358a831eb53ef834
twoo0220 <twoo0220@gmail.com>
2024-09-04 01:04:12 +0900

    24.09.04 - arithmetic

---
+++ BOJ/C++/5063.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count(0), r(0), e(0), c(0);
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		std::cin >> r >> e >> c;
+		int result = e - r - c;
+		if (result > 0)
+		{
+			std::cout << "advertise\n";
+		}
+		else if (result == 0)
+		{
+			std::cout << "does not matter\n";
+		}
+		else
+		{
+			std::cout << "do not advertise\n";
+		}
+	}
+
+	return 0;
+}
