e322455129f639ca5325a539e66a8a9b1475efca
twoo0220 <twoo0220@gmail.com>
2024-09-05 00:50:25 +0900

    24.09.05 - arithmetic

---
+++ BOJ/C++/5717.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		int M(0), F(0);
+		std::cin >> M >> F;
+
+		if (M == 0)
+		{
+			break;
+		}
+		std::cout << (M + F) << '\n';
+	}
+
+	return 0;
+}
