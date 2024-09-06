d73da0f159a65a59548c2832d82c47be37fae5dc
twoo0220 <twoo0220@gmail.com>
2024-09-06 23:39:30 +0900

    24.09.06 - arithmetic

---
+++ BOJ/C++/10824.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long a(0), b(0), c(0), d(0);
+	std::cin >> a >> b >> c >> d;
+
+	long long temp = b;
+	while (true)
+	{
+		a *= 10;
+		if (temp / 10 == 0)
+		{
+			break;
+		}
+
+		temp /= 10;
+	}
+
+	temp = d;
+	while (true)
+	{
+		c *= 10;
+		if (temp / 10 == 0)
+		{
+			break;
+		}
+
+		temp /= 10;
+	}
+	
+	std::cout << (a + b + c + d);
+	return 0;
+}
