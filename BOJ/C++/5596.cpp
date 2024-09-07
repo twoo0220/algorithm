57572b4f5ae4539e4329f0bacc55b0a9a3e3110f
twoo0220 <twoo0220@gmail.com>
2024-09-08 03:22:09 +0900

    24.09.08 - arithmetic

---
+++ BOJ/C++/5596.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int sum[2]{};
+	for (int i = 0; i < 2; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			int temp = 0;
+			std::cin >> temp;
+			sum[i] += temp;
+		}
+	}
+
+	if (sum[0] >= sum[1])
+	{
+		std::cout << sum[0];
+	}
+	else
+	{
+		std::cout << sum[1];
+	}
+	return 0;
+}
