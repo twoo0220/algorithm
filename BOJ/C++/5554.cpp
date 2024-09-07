959b42068c7194dea47c10b98ab6ddf57c759e6c
twoo0220 <twoo0220@gmail.com>
2024-09-07 23:56:51 +0900

    24.09.07 - arithmetic

---
+++ BOJ/C++/5554.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int min(0), sec(0), sum(0);
+	for (int i = 0; i < 4; ++i)
+	{
+		int temp = 0;
+		std::cin >> temp;
+		sum += temp;
+	}
+
+	min = sum / 60;
+	sec = sum - (min * 60);
+	std::cout << min << '\n' << sec;
+	return 0;
+}
