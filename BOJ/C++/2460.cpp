31267ac33d965b071685b79e867193500810f2d3
twoo0220 <twoo0220@gmail.com>
2024-09-12 00:49:58 +0900

    24.09.12 - arithmetic

---
+++ BOJ/C++/2460.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+	
+	int current(0), max(0);
+	for (int i = 0; i < 10; ++i)
+	{
+		int in(0), out(0);
+		std::cin >> out >> in;
+		current += (in - out);
+
+		if (current > max)
+		{
+			max = current;
+		}
+	}
+
+	std::cout << max;
+
+	return 0;
+}
