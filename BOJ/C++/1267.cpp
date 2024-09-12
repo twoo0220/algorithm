80c8f4e21ecd3f2a86eb061229a355a3f4c97558
twoo0220 <twoo0220@gmail.com>
2024-09-13 01:05:36 +0900

    24.09.13 - arithmetic

---
+++ BOJ/C++/1267.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size(0), Y(0), M(0);
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int time = 0;
+		std::cin >> time;
+
+		Y += ((time / 30) + 1) * 10;
+		M += ((time / 60) + 1) * 15;
+	}
+
+	if (Y > M)
+	{
+		std::cout << "M " << M;
+	}
+	else if (Y == M)
+	{
+		std::cout << "Y M " << Y;
+	}
+	else
+	{
+		std::cout << "Y " << Y;
+	}
+
+	return 0;
+}
