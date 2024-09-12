0b3a7b0d9090f2335a724466f27464ea92a7c389
twoo0220 <twoo0220@gmail.com>
2024-09-13 00:29:55 +0900

    24.09.13 - arithmetic

---
+++ BOJ/C++/15964.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+	
+	long long a(0), b(0);
+	std::cin >> a >> b;
+	std::cout << (a + b) * (a - b);
+
+	return 0;
+}
