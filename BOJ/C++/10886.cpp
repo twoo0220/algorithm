cf8882c45c319b129e73a8fadae01889235a1657
twoo0220 <twoo0220@gmail.com>
2024-09-14 02:25:11 +0900

    24.09.14 - arithmetic

---
+++ BOJ/C++/10886.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size(0), a(0), b(0);
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int vote = 0;
+		std::cin >> vote;
+		
+		if (vote == 1)
+		{
+			a++;
+		}
+		else
+		{
+			b++;
+		}
+	}
+
+	if (a > b)
+	{
+		std::cout << "Junhee is cute!";
+	}
+	else
+	{
+		std::cout << "Junhee is not cute!";
+	}
+
+	return 0;
+}
