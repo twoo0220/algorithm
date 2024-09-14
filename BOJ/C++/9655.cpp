b4bc2740bccfda766b9a86fb6fa6b4ee7738f6fc
twoo0220 <twoo0220@gmail.com>
2024-09-15 01:58:31 +0900

    24.09.15 - arithmetic

---
+++ BOJ/C++/9655.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	if ((size % 2) == 0)
+	{
+		std::cout << "CY";
+	}
+	else
+	{
+		std::cout << "SK";
+	}
+
+	return 0;
+}
