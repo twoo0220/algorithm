237fb47f403acb28236e6b94ca28287b44c2f73e
twoo0220 <twoo0220@gmail.com>
2024-09-16 03:17:12 +0900

    24.09.16 - arithmetic

---
+++ BOJ/C++/2875.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int n(0), m(0), k(0);
+	std::cin >> n >> m >> k;
+
+	int result = n / 2 > m ? m : n / 2;
+	k -= (n + m) - 3 * result;
+
+	if (k > 0)
+	{
+		result -= (--k / 3) + 1;
+	}
+
+	std::cout << result;
+
+	return 0;
+}
