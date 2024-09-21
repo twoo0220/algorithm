a8d05dc3083d3c1f107bb53e87d7ae98e54d584a
twoo0220 <twoo0220@gmail.com>
2024-09-21 21:13:42 +0900

    24.09.21 - arithmetic

---
+++ BOJ/C++/1075.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int N(0), F(0), result(0);
+	std::cin >> N >> F;
+
+	N /= 100;
+	N *= 100;
+	
+	while (true)
+	{
+		if ((((N + result) % F) == 0) || (result > 100))
+		{
+			break;
+		}
+		result++;
+	}
+
+	if (result < 10)
+	{
+		std::cout << "0";
+	}
+	std::cout << result;
+
+	return 0;
+}
