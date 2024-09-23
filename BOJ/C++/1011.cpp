ca23835f3a149bb1d2c383a81ef1acb06fff1625
twoo0220 <twoo0220@gmail.com>
2024-09-23 23:29:20 +0900

    24.09.23 - arithmetic

---
+++ BOJ/C++/1011.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size(0);
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int start(0), end(0);
+		std::cin >> start >> end;
+
+		int length = end - start;
+		int result = static_cast<int>(std::sqrt(length - 1));
+		if ((length - (result * result)) <= result)
+		{
+			std::cout << 2 * result << "\n";
+		}
+		else
+		{
+			std::cout << (2 * result) + 1 << "\n";
+		}
+
+	}
+
+	return 0;
+}
