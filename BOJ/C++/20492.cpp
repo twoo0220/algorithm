9d07b53421b411bbd7bf50b4415c1e2739094922
twoo0220 <twoo0220@gmail.com>
2024-09-25 03:08:30 +0900

    24.09.25 - arithmetic

---
+++ BOJ/C++/20492.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int money = 0;
+	std::cin >> money;
+	std::cout << money * 78 / 100 << " " << (money * 8 / 10) + (money * 156 / 1000);
+
+	return 0;
+}
