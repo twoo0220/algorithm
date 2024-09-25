b75b7066025d9d48e4237af81d4f21c6fd6b58a6
twoo0220 <twoo0220@gmail.com>
2024-09-26 00:36:34 +0900

    24.09.26 - arithmetic

---
+++ BOJ/C++/2476.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::vector<int> dice(3);
+	int size = 0;
+	std::cin >> size;
+
+	int max = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		std::cin >> dice[0] >> dice[1] >> dice[2];
+		std::sort(dice.begin(), dice.end());
+
+		int result = 0;
+		if (dice[0] == dice[1])
+		{
+			if (dice[1] == dice[2])
+			{
+				result = 10000 + dice[2] * 1000;
+			}
+			else
+			{
+				result = 1000 + dice[1] * 100;
+			}
+		}
+		else
+		{
+			if (dice[1] == dice[2])
+			{
+				result = 1000 + dice[2] * 100;
+			}
+			else
+			{
+				result = dice[2] * 100;
+			}
+		}
+
+		if (result > max)
+		{
+			max = result;
+		}
+	}
+
+	std::cout << max;
+	return 0;
+}
