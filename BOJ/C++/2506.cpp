17f5fa82190d9904b969c070d1a31e56b2a1b83f
twoo0220 <twoo0220@gmail.com>
2024-09-30 02:08:38 +0900

    24.09.30 - arithmetic

---
+++ BOJ/C++/2506.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	int score(0), result(0);
+	for (int i = 0; i < count; ++i)
+	{
+		int OX = 0;
+		std::cin >> OX;
+		
+		if (OX == 1)
+		{
+			score++;
+			result += score;
+		}
+		else
+		{
+			score = 0;
+		}
+	}
+
+	std::cout << result;
+	return 0;
+}
