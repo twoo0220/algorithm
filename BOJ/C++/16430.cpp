ec1d81c5bb8c58a36558152379de197a26788074
twoo0220 <twoo0220@gmail.com>
2024-10-01 02:23:53 +0900

    24.10.01 - arithmetic

---
+++ BOJ/C++/16430.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(1);
+	std::cin >> a >> b;
+	std::cout << (b - a) << " " << b;
+
+	return 0;
+}
