46e42c8dadd1cf7ced50feaf55af4cb4adb0bade
twoo0220 <twoo0220@gmail.com>
2024-10-08 22:49:12 +0900

    24.10.08 - arithmetic

---
+++ BOJ/C++/5355.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		double number = 0;
+		std::cin >> number;
+
+		while (true)
+		{
+			char ch;
+			std::cin.get(ch);
+
+			if (ch == '\n') break;
+			if (ch == '@') number *= 3;
+			if (ch == '%') number += 5;
+			if (ch == '#') number -= 7;
+		}
+
+		std::cout << std::fixed;
+		std::cout.precision(2);
+		std::cout << number << "\n";
+	}
+	return 0;
+}
