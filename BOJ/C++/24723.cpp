9730c9f5456e530a033d2006b8ffcc5eaa173d06
twoo0220 <twoo0220@gmail.com>
2024-10-09 15:59:05 +0900

    24.10.09 - arithmetic

---
+++ BOJ/C++/24723.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+	std::cout << std::pow(2, size);
+	return 0;
+}
