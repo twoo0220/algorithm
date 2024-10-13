b68b2718c54e7d7e38f26869bf92b568b8c8034c
twoo0220 <twoo0220@gmail.com>
2024-10-14 00:21:27 +0900

    24.10.14 - arithmetic, formation

---
+++ BOJ/C++/1550.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> std::hex >> number;
+	std::cout << number;
+
+	return 0;
+}
