b19f0646919d28459bc06e2df3f6448a8524fbb8
twoo0220 <twoo0220@gmail.com>
2024-10-17 01:19:34 +0900

    24.10.17 - arithmetic

---
+++ BOJ/C++/6749.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int young(0), middle(0);
+	std::cin >> young >> middle;
+	std::cout << (middle + (middle - young));
+	return 0;
+}
