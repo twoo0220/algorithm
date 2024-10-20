7e61a587023dc4b7e0efe614ab52db01779ddb93
twoo0220 <twoo0220@gmail.com>
2024-10-20 21:31:46 +0900

    24.10.20 - arithmetic, time

---
+++ BOJ/C++/2530.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int hour(0), min(0), sec(0), time(0);
+	std::cin >> hour >> min >> sec >> time;
+
+	sec += time;
+	min += sec / 60;
+	sec %= 60;
+	hour += min / 60;
+	min %= 60;
+	hour %= 24;
+	
+	std::cout << hour << " " << min << " " << sec;
+	return 0;
+}
