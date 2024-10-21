d0bdcb33f2f335ff3267d0fc497543f16319dd10
twoo0220 <twoo0220@gmail.com>
2024-10-22 03:22:28 +0900

    24.10.22 - arithmetic

---
+++ BOJ/C++/10833.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	int remain = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		int student(0), apple(0);
+		std::cin >> student >> apple;
+		remain += apple - student * (apple / student);
+	}
+
+	std::cout << remain;
+	return 0;
+}
