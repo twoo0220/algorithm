798173c55c47989d19cb68cc1066e576ff0a9d4c
twoo0220 <twoo0220@gmail.com>
2024-10-23 02:50:00 +0900

    24.10.23 - arithmetic, geometry

---
+++ BOJ/C++/2477.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int korean_melon = 0;
+	std::cin >> korean_melon;
+
+	int direction[6]{};
+	int size[6]{};
+
+	for (int i = 0; i < 6; ++i)
+	{
+		std::cin >> direction[i] >> size[i];
+	}
+
+	int maxArea(0), index(0);
+	for (int i = 0; i < 6; i++)
+	{
+		if (maxArea < size[i] * size[(i + 1) % 6])
+		{
+			maxArea = size[i] * size[(i + 1) % 6];
+			index = i;
+		}
+	}
+
+	int minArea = size[(index + 3) % 6] * size[(index + 4) % 6];
+	std::cout << (maxArea - minArea) * korean_melon;
+	return 0;
+}
