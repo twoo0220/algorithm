103dc0e635260e13343254a21e29d6151261297b
twoo0220 <twoo0220@gmail.com>
2024-10-24 02:54:19 +0900

    24.10.24 - arithmetic

---
+++ BOJ/C++/9085.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int count(0), sum(0);
+		std::cin >> count;
+		for (int j = 0; j < count; ++j)
+		{
+			int number = 0;
+			std::cin >> number;
+			sum += number;
+		}
+
+		std::cout << sum << "\n";
+	}
+
+	return 0;
+}
