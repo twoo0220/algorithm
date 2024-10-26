2fa3d0f78cb100ffaa1c20cee611b5cb0025c19b
twoo0220 <twoo0220@gmail.com>
2024-10-27 01:53:08 +0900

    24.10.27 - arithmetic

---
+++ BOJ/C++/1748.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> number;
+
+	int index = 10;
+	int count = 1;
+	long long result = 0;
+	for (int i = 1; i <= number; ++i)
+	{
+		if (i >= index)
+		{
+			index *= 10;
+			count++;
+		}
+		result += count;
+	}
+
+	std::cout << result;
+	return 0;
+}
