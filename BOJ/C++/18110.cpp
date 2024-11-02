2470914a7e3ad8114f9e6bd2b5a3fb15a2ac9979
twoo0220 <twoo0220@gmail.com>
2024-11-03 03:15:49 +0900

    24.11.03 - arithmetic

---
+++ BOJ/C++/18110.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	if (size == 0)
+	{
+		std::cout << 0;
+		return 0;
+	}
+	
+	std::vector<int> arr(size);
+	for (int i = 0; i < size; ++i)
+	{
+		std::cin >> arr[i];
+	}
+
+	std::sort(arr.begin(), arr.end());
+	int index = std::round(size * 0.15);
+	double sum = 0;
+
+	for (int i = index; i < size - index; ++i)
+	{
+		sum += arr[i];
+	}
+
+	int average = std::round(sum / (size - index * 2));
+	std::cout << average;
+	return 0;
+}
