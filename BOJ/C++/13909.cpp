168e7b41f10d7a4a8d85b9f24d43b07056f57f96
twoo0220 <twoo0220@gmail.com>
2024-11-04 00:39:37 +0900

    24.11.04 - arithmetic, Number Theory

---
+++ BOJ/C++/13909.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	int count = 0;
+	for (int i = 1; i * i <= size; ++i)
+	{
+		count++;
+	}
+
+	std::cout << count;
+	return 0;
+}
