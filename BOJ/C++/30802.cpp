81afd04c455c5a06b39042075f687d5fddc91e54
twoo0220 <twoo0220@gmail.com>
2024-11-05 02:11:47 +0900

    24.11.05 - arithmetic

---
+++ BOJ/C++/30802.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int arr[6]{};
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < 6; ++i)
+	{
+		std::cin >> arr[i];
+	}
+
+	int T(0), P(0);
+	std::cin >> T >> P;
+
+	int result = 0;
+	for (int i = 0; i < 6; ++i)
+	{
+		if (arr[i] % T == 0)
+		{
+			result += arr[i] / T;
+		}
+		else
+		{
+			result += arr[i] / T + 1;
+		}
+	}
+	
+	std::cout << result << "\n" << size / P << " " << size % P;
+	return 0;
+}
