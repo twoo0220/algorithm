41e47883a18f2ce9583cca1a2713269aefab697e
twoo0220 <twoo0220@gmail.com>
2024-11-06 01:32:16 +0900

    24.11.06 - arithmetic

---
+++ BOJ/C++/11943.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0), d(0);
+	std::cin >> a >> b >> c >> d;
+
+	if ((a + d) > (b + c))
+	{
+		std::cout << (b + c);
+	}
+	else
+	{
+		std::cout << (a + d);
+	}
+
+	return 0;
+}
