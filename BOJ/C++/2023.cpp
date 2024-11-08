94c7a3c1dde13b2a44bc5822dbf2c588da1bf896
twoo0220 <twoo0220@gmail.com>
2024-11-09 00:24:03 +0900

    24.11.09 - DFS, prime number, back tracking

---
+++ BOJ/C++/2023.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+static bool isPrimeNumber(int number)
+{
+	if (number < 2)
+	{
+		return false;
+	}
+
+	for (int i = 2; i * i <= number; ++i)
+	{
+		if ((number % i) == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static void DFS(int number, int size)
+{
+	if (size == 0)
+	{
+		std::cout << number << "\n";
+		return;
+	}
+
+	for (int i = 1; i < 10; ++i)
+	{
+		int temp = 10 * number + i;
+		if ((size > 0) && (isPrimeNumber(temp)))
+		{
+			DFS(temp, size - 1);
+		}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	DFS(0, size);
+
+	return 0;
+}
