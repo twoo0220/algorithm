3fb5521bfc9d63f6456763e520b1e9526355f418
twoo0220 <twoo0220@gmail.com>
2024-11-09 13:31:21 +0900

    24.11.09 - arithmetic

---
+++ BOJ/C++/2720.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	const int change[4] = { 25, 10, 5, 1 };
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int number = 0;
+		std::cin >> number;
+
+		int index = 0;
+		while (index < 4)
+		{
+			std::cout << number / change[index] << " ";
+			number %= change[index];
+			index++;
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+}
