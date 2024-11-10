359c8a4f37e207814b6e77fd3d5faea7e141658a
twoo0220 <twoo0220@gmail.com>
2024-11-11 01:22:24 +0900

    24.11.11 - arithmetic

---
+++ BOJ/C++/1977.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int M(0), N(0);
+	std::cin >> M >> N;
+
+	int min = static_cast<int>(std::sqrt(M));
+	if (min * min < M)
+	{
+		min++;
+	}
+
+	int max = static_cast<int>(std::sqrt(N));
+	if (max * max < N)
+	{
+		max++;
+	}
+
+	int sum = 0;
+	for (int i = min; i * i <= N; ++i)
+	{
+		sum += i * i;
+	}
+
+	if (sum != 0)
+	{
+		std::cout << sum << "\n";
+		std::cout << min * min;
+	}
+	else
+	{
+		std::cout << -1;
+	}
+	
+	return 0;
+}
