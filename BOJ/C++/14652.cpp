27b85dcb16ad0db7db59cb4cb8bd74d326d5a924
twoo0220 <twoo0220@gmail.com>
2024-11-13 00:50:13 +0900

    24.11.13 - arithmetic

---
+++ BOJ/C++/14652.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0);
+	std::cin >> a >> b >> c;
+	std::cout << (c / b) << " " << (c % b);
+
+	return 0;
+}
