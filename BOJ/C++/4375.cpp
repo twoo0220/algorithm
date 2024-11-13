fa4445268c206af38844e283728d2f41b5f519eb
twoo0220 <twoo0220@gmail.com>
2024-11-14 00:51:51 +0900

    24.11.14 - arithmetic, mod, modular

---
+++ BOJ/C++/4375.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 1;
+	while ((std::cin >> number))
+	{
+		int result = 1;
+		int count = 1;
+		while (true)
+		{
+			if ((result % number) == 0)
+			{
+				break;
+			}
+
+			result = result * 10 + 1;
+			result %= number;
+			count++;
+		}
+		std::cout << count << "\n";
+	}
+
+	return 0;
+}
