da9f20a431cd472a62c323f5162a497928e2714b
twoo0220 <twoo0220@gmail.com>
2024-11-14 00:26:33 +0900

    24.11.14 - arithmetic, gcd(Greatest Common Divisor)

---
+++ BOJ/C++/3036.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+static int gcd(int a, int b)
+{
+	while (b != 0)
+	{
+		int temp = a % b;
+		a = b;
+		b = temp;
+	}
+
+	return a;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	int standard = 0;
+	std::cin >> standard;
+
+	for (int i = 1; i < count; ++i)
+	{
+		int circle = 0;
+		std::cin >> circle;
+		int result = gcd(standard, circle);
+
+		std::cout << (standard / result) << "/" << (circle / result) << "\n";
+	}
+	return 0;
+}
