17baff8fb7929c246320dd8c621fbc9b1a2d444a
twoo0220 <twoo0220@gmail.com>
2024-11-15 02:00:42 +0900

    24.11.15 - arithmetic

---
+++ BOJ/C++/2018.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> number;
+
+	int count = 0;
+	for (int i = number; i > 0; --i)
+	{
+		int sum = 0;
+		for (int j = i; j > 0; --j)
+		{
+			sum += j;
+			if (sum == number)
+			{
+				count++;
+				break;
+			}
+			
+			if (sum > number)
+			{
+				break;
+			}
+		}
+	}
+
+	std::cout << count;
+	return 0;
+}
