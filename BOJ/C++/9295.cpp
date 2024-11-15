c544db6f6dbdec52b187c391945f51dfce467cd9
twoo0220 <twoo0220@gmail.com>
2024-11-16 00:33:17 +0900

    24.11.16 - arithmetic

---
+++ BOJ/C++/9295.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int a(0), b(0);
+		std::cin >> a >> b;
+		std::cout << "Case " << (i + 1) << ": " << (a + b) << "\n";
+	}
+	return 0;
+}
