f6d87c604dfc1b0978df86fb18dde9070de3235d
twoo0220 <twoo0220@gmail.com>
2024-11-16 00:35:47 +0900

    24.11.16 - arithmetic

---
+++ BOJ/C++/5341.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	while (true)
+	{
+		int size = -1;
+		std::cin >> size;
+
+		if (size == 0) break;
+
+		int sum = 0;
+		for (int i = 1; i <= size; ++i)
+		{
+			sum += i;
+		}
+		std::cout << sum << "\n";
+	}
+	return 0;
+}
