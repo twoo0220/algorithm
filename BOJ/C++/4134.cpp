df9264246321abab2c5d29c50bf8d75f3c3f582b
twoo0220 <twoo0220@gmail.com>
2024-11-16 00:25:39 +0900

    24.11.16 - eratos, prime number

---
+++ BOJ/C++/4134.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+static bool isPrimeNumber(long long number)
+{
+	if (number < 2)
+	{
+		return false;
+	}
+
+	for (long long i = 2; i * i <= number; ++i)
+	{
+		if ((number % i) == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long size(0), number(0);
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		std::cin >> number;
+
+		while (true)
+		{
+			if (isPrimeNumber(number))
+			{
+				break;
+			}
+			number++;
+		}
+		std::cout << number << "\n";
+	}
+
+	return 0;
+}
