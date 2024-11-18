526d306e7e3120891617b7f8794b2904d9fa35d4
twoo0220 <twoo0220@gmail.com>
2024-11-19 00:04:29 +0900

    24.11.19 - arithmetic

---
+++ BOJ/C++/6359.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int size = 0;
+		std::cin >> size;
+
+		int index = 1;
+		while (true)
+		{
+			if (index * index > size)
+			{
+				std::cout << index - 1 << "\n";
+				break;
+			}
+			index++;
+		}
+	}
+	return 0;
+}
