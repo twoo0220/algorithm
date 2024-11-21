b7b14269027f1401b0866535e4a786b0f339e4d5
twoo0220 <twoo0220@gmail.com>
2024-11-22 02:23:57 +0900

    24.11.22 - arithmetic

---
+++ BOJ/C++/2331.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::vector<int> vec;
+	int number(0), P(0), count(0);
+	std::cin >> number >> P;
+
+	vec.push_back(number);
+	
+	while (true)
+	{
+		count = 0;
+		int tempNumber = 0;
+		while (number != 0)
+		{
+			tempNumber += std::pow(number % 10, P);
+			number /= 10;
+		}
+
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			if (vec[i] == tempNumber)
+			{
+				goto out;
+			}
+			count++;
+		}
+		number = tempNumber;
+		vec.push_back(number);
+	}
+	
+out:
+	std::cout << count;
+	return 0;
+}
