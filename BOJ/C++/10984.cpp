981eb5bb8b865a10dab542d286bab438d9b69218
twoo0220 <twoo0220@gmail.com>
2024-11-24 01:08:54 +0900

    24.11.24 - arithmetic

---
+++ BOJ/C++/10984.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size; 
+
+	std::cout.precision(2);
+	for (int i = 0; i < size; ++i)
+	{
+		int count = 0;
+		std::cin >> count;
+
+		float sum(0), total(0);
+		for (int j = 0; j < count; ++j)
+		{
+			float grade(0), score(0);
+			std::cin >> grade >> score;
+			sum += grade;
+			total += (grade * score);
+		}
+
+		std::cout << static_cast<int>(sum) << " " << (total / sum) << "\n";
+	}
+	return 0;
+}
