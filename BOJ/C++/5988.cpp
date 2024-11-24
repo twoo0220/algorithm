e599f31fb626f3d3a33e3fc7fb3b3ee1085b0945
twoo0220 <twoo0220@gmail.com>
2024-11-25 01:55:02 +0900

    24.11.25 - arithmetic, string

---
+++ BOJ/C++/5988.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		std::string number;
+		std::cin >> number;
+		int lastNumber = number[number.size() - 1] - 48;
+
+		if (lastNumber % 2 == 0)
+		{
+			std::cout << "even\n";
+		}
+		else
+		{
+			std::cout << "odd\n";
+		}
+	}
+	return 0;
+}
