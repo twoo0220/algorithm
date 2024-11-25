ea55e1bbf6cf078dd7f5f1368ed1ebc6032f4932
twoo0220 <twoo0220@gmail.com>
2024-11-26 01:42:51 +0900

    24.11.26 - arithmetic

---
+++ BOJ/C++/15726.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	double a(0), b(0), c(0);
+	std::cin >> a >> b >> c;
+
+	if (b > c)
+	{
+		std::cout << static_cast<int>(a * b / c);
+	}
+	else
+	{
+		std::cout << static_cast<int>(a / b * c);
+	}
+
+	return 0;
+}
