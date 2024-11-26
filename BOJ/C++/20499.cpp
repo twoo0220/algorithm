603b2b913efaf76f0069bf783d725df7765093ed
twoo0220 <twoo0220@gmail.com>
2024-11-27 00:08:03 +0900

    24.11.27 - arithmetic

---
+++ BOJ/C++/20499.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0);
+	char dummy;
+	std::cin >> a >> dummy >> b >> dummy >> c;
+	
+	if (((a + c) < b) || (b == 0))
+	{
+		std::cout << "hasu";
+	}
+	else
+	{
+		std::cout << "gosu";
+	}
+
+	return 0;
+}
---
+++ BOJ/C++/2965.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0);
+	std::cin >> a >> b >> c;
+	
+	int temp = b - a;
+	if (temp > (c - b))
+	{
+		std::cout << (temp - 1);
+	}
+	else
+	{
+		std::cout << (c - b - 1);
+	}
+	return 0;
+}
