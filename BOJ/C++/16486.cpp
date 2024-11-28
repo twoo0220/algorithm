4fe2bd3b5b8a487362ebe793b2ecb9ab0db7a8a3
twoo0220 <twoo0220@gmail.com>
2024-11-29 00:47:10 +0900

    24.11.29 - arithmetic

---
+++ BOJ/C++/16486.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	double d1(0), d2(0);
+	std::cin >> d1 >> d2;
+
+	double pi = 3.141592;
+	std::cout.precision(7);
+	std::cout << (2 * pi * d2) + (2 * d1);
+
+	return 0;
+}
