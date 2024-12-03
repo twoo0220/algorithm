5c0e8b9d1630cc00a7293ad7719005553bc981d5
twoo0220 <twoo0220@gmail.com>
2024-12-04 01:51:25 +0900

    24.12.4 - arithmetic

---
+++ BOJ/C++/4504.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int number = 0;
+    std::cin >> number;
+
+    while (true)
+    {
+        int temp = 0;
+        std::cin >> temp;
+
+        if (temp == 0)
+        {
+            break;
+        }
+
+        if (temp % number == 0)
+        {
+            std::cout << temp << " is a multiple of " << number << ".\n";
+        }
+        else
+        {
+            std::cout << temp << " is NOT a multiple of " << number << ".\n";
+        }
+    }
+    return 0;
+}
