41f28c4fb9ea5120cec2b68fd965f1d3eeb71ca4
twoo0220 <twoo0220@gmail.com>
2024-12-03 23:27:46 +0900

    24.12.3 - arithmetic

---
+++ BOJ/C++/10178.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int size = 0;
+    std::cin >> size;
+
+    for (int i = 0; i < size; ++i)
+    {
+        int a(0), b(0);
+        std::cin >> a >> b;
+        std::cout << "You get " << a / b << " piece(s) and your dad gets " << a % b << " piece(s).\n";
+    }
+
+    return 0;
+}
---
+++ BOJ/C++/1408.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int hour(0), min(0), sec(0), a(0), b(0), c(0);
+	char temp(0);
+	std::cin >> hour >> temp >> min >> temp >> sec;
+	std::cin >> a >> temp >> b >> temp >> c;
+	
+	int remainTime = (a - hour) * 3600 + (b - min) * 60 + (c - sec);
+	if (remainTime < 0)
+	{
+		remainTime += 24 * 3600;
+	}
+	printf("%02d:%02d:%02d", remainTime / 3600, remainTime % 3600 / 60, remainTime % 60);
+	
+	return 0;
+}
