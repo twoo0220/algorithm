c306c46d9ca9694b5f46262f21a07b34921ce6b3
twoo0220 <twoo0220@gmail.com>
2024-12-05 01:11:14 +0900

    24.12.5 - arithmetic

---
+++ BOJ/C++/18310.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int count = 0;
+    std::cin >> count;
+
+    std::vector<int> vec;
+    vec.reserve(count);
+
+    for (int i = 0; i < count; ++i)
+    {
+        int number = 0;
+        std::cin >> number;
+        vec.push_back(number);
+    }
+
+    std::sort(vec.begin(), vec.end());
+    std::cout << vec[(count - 1) / 2];
+
+    return 0;
+}
