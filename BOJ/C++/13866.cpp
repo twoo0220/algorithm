c6c0c1d6c86c1750b101ca9f856a3d049cc02383
twoo0220 <twoo0220@gmail.com>
2024-12-06 00:04:36 +0900

    24.12.06 - arithmetic

---
+++ BOJ/C++/13866.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int a(0), b(0), c(0), d(0);
+    std::cin >> a >> b >> c >> d;
+    std::cout << std::abs(a + d - b - c);
+
+    return 0;
+}
