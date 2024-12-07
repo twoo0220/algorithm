80d524cce69fb0c96fcb8776b8189ca61b6577d0
twoo0220 <twoo0220@gmail.com>
2024-12-07 21:33:11 +0900

    24.12.07 - arithmetic

---
+++ BOJ/C++/1568.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int size(0), count(0), time(0);
+    std::cin >> size;
+
+    while (true)
+    {
+        count++;
+        if (size == 0) break;
+        if (size < count) count = 1;
+        size -= count;
+        time++;
+    }
+
+    std::cout << time;
+    return 0;
+}
