1383813eff2a807ee69a25859825563c90e6ee1e
twoo0220 <twoo0220@gmail.com>
2024-12-09 23:04:28 +0900

    24.12.09 - arithmetic

---
+++ BOJ/C++/3034.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count(0), width(0), height(0);
+	std::cin >> count >> width >> height;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int size = 0;
+		std::cin >> size;
+
+		if ((size <= width) ||
+			(size <= height) ||
+			(size <= std::sqrt(width * width + height * height)))
+		{
+			std::cout << "DA\n";
+		}
+		else
+		{
+			std::cout << "NE\n";
+		}
+	}
+
+	return 0;
+}
