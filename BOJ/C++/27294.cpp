69928ee50ea0d0ad7a19ea6e37124340c4c86dae
twoo0220 <twoo0220@gmail.com>
2024-12-12 02:25:11 +0900

    24.12.12 - arithmetic

---
+++ BOJ/C++/27294.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int a(0), b(0);
+    std::cin >> a >> b;
+
+    if (a >= 12 && a <= 16 && b < 1)
+    {
+        std::cout << 320;
+    }
+    else
+    {
+        std::cout << 280;
+    }
+
+    return 0;
+}
