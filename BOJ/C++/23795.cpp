69307b7896ecfb9ebe4d55a40d3ec221a7d53b67
twoo0220 <twoo0220@gmail.com>
2024-12-24 01:46:28 +0900

    24.12.24 - arithmetic

---
+++ BOJ/C++/23795.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int sum = 0;
+    while (true)
+    {
+        int number = 0;
+        std::cin >> number;
+        if (number == -1) break;
+
+        sum += number;
+    }
+
+    std::cout << sum;
+    return 0;
+}
