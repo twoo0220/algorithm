d6e14cbf26cf45502ca167365c64f871eced17ee
twoo0220 <twoo0220@gmail.com>
2024-12-25 02:20:05 +0900

    24.12.25 - arithmetic

---
+++ BOJ/C++/1964.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int size = 0;
+    std::cin >> size;
+
+    long long prev(5), curr(5);
+    for (int i = 2; i <= size; ++i)
+    {
+        curr = 3 * i + prev + 1;
+        prev = curr;
+    }
+
+    std::cout << (curr % 45678);
+    return 0;
+}
