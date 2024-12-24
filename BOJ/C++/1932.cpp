19395c7bd4d9d2cc9ee5a52bb73973ad37c66da5
twoo0220 <twoo0220@gmail.com>
2024-12-25 02:59:30 +0900

    24.12.25 - arithmetic, dynamic programming

---
+++ BOJ/C++/1932.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	int* triangle = new int[501] {};
+	int* prevTri = new int[501] {};
+
+	int max = 0;
+	for (int i = 1; i <= size; ++i)
+	{
+		int number = 0;
+		std::cin >> number;
+		triangle[1] += number;
+
+		for (int j = 2; j <= i; ++j)
+		{
+			std::cin >> number;
+			triangle[j] = prevTri[j - 1] > prevTri[j] ? prevTri[j - 1] + number : prevTri[j] + number;
+		}
+
+		for (int j = 1; j <= i; ++j)
+		{
+			prevTri[j] = triangle[j];
+			if (prevTri[j] > max)
+			{
+				max = prevTri[j];
+			}
+		}
+	}
+
+	std::cout << max;
+	delete[] prevTri;
+	delete[] triangle;
+	return 0;
+}
