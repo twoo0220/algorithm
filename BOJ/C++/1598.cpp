e4542ba91865d91bb7c4e0044bb729ae43d34d41
twoo0220 <twoo0220@gmail.com>
2024-12-27 02:33:22 +0900

    24.12.27 - arithmetic

---
+++ BOJ/C++/1598.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0);
+	std::cin >> a >> b;
+	std::cout << std::abs((a - 1) / 4 - (b - 1) / 4) + std::abs((a - 1) % 4 - (b - 1) % 4);
+	
+	return 0;
+}
