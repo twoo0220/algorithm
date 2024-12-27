33a862a6a9a1438fcdab76d5e6e91dd3c8fdd34a
twoo0220 <twoo0220@gmail.com>
2024-12-28 01:54:25 +0900

    24.12.28 - math, dynamic programming

---
+++ BOJ/C++/16395.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int n(0), k(0);
+    std::cin >> n >> k;
+
+    int triangle[31]{};
+    int prevTri[31]{};
+
+    triangle[1] = 1;
+    prevTri[1] = 1;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        triangle[i] = 1;
+        for (int j = 2; j < i; ++j)
+        {
+            triangle[j] = prevTri[j - 1] + prevTri[j];
+        }
+
+        for (int j = 1; j <= i; ++j)
+        {
+            prevTri[j] = triangle[j];
+        }
+    }
+
+    std::cout << triangle[k];
+
+    return 0;
+}
