e38cd4f0613cfca6f0184200b34d70a050c0e42b
twoo0220 <twoo0220@gmail.com>
2024-12-29 02:57:52 +0900

    24.12.29 - arithmetic

---
+++ BOJ/C++/1526.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int number = 0;
+    std::cin >> number;
+
+    for (int i = number; i >= 4; --i)
+    {
+        int temp = i;
+
+        while (temp != 0)
+        {
+            int a = temp % 10;
+            if ((a != 4) && (a != 7))
+            {
+                break;
+            }
+            temp /= 10;
+        }
+
+        if (temp == 0)
+        {
+            std::cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
