b4eb25c77f0966125615d0f025c495acd4c82865
twoo0220 <twoo0220@gmail.com>
2025-01-03 00:54:10 +0900

    25.01.03 - math, Euler phi function

---
+++ BOJ/C++/11689.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+	long long n(0), count(0);
+	std::cin >> n;
+	count = n;
+
+	for (long long i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+		{
+			count -= count / i;
+			while (n % i == 0)
+			{
+				n /= i;
+			}
+		}
+	}
+
+	if (n > 1)
+	{
+		count -= count / n;
+	}
+
+	std::cout << count;
+    return 0;
+}
