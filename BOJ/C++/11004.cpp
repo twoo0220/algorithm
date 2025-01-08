09ffb8750a1fa4efaa8c1ec07949bf8d479500b6
twoo0220 <twoo0220@gmail.com>
2025-01-08 23:18:46 +0900

    25.01.08 - sort

---
+++ BOJ/C++/11004.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int arrSize(0), index(0);
+    std::cin >> arrSize >> index;
+    std::vector<int> arr(arrSize, 0);
+	
+	for (int i = 0; i < arrSize; ++i)
+	{
+        std::cin >> arr[i];
+	}
+
+	std::sort(arr.begin(), arr.end());
+    std::cout << arr[index - 1];
+
+    return 0;
+}
