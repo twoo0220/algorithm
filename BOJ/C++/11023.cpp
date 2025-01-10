a637b6e248fb93e1d1a52ce549dd11a375cedde4
twoo0220 <twoo0220@gmail.com>
2025-01-11 01:57:25 +0900

    25.01.11 - arithmetic

---
+++ BOJ/C++/11023.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int sum(0), number(0);
+
+    while (std::cin >> number)
+    {
+        sum += number;
+    }
+    std::cout << sum;
+
+    return 0;
+}
