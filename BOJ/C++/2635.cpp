1bbfb19663f8ec03fde4e35ab0a86075c42b80ec
twoo0220 <twoo0220@gmail.com>
2025-01-13 02:02:06 +0900

    25.01.13 - math, brute force

---
+++ BOJ/C++/2635.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int number = 0;
+	std::cin >> number;
+    std::vector<int> arr;
+
+    for (int i = number / 2 + 1; i <= number; ++i)
+    {
+        std::vector<int> temp;
+        temp.push_back(number);
+		temp.push_back(i);
+        
+        int j = 1;
+        while (true)
+        {
+            if (temp[j - 1] - temp[j] >= 0)
+            {
+				temp.push_back(temp[j - 1] - temp[j]);
+            }
+            else
+            {
+                break;
+            }
+            ++j;
+        }
+
+        if (temp.size() > arr.size())
+        {
+            arr = temp;
+        }
+    }
+
+    std::cout << arr.size() << '\n';
+    for (const auto i : arr)
+    {
+        std::cout << i << ' ';
+    }
+
+    return 0;
+}
