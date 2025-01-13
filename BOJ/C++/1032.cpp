3cc5e28bda9503dfba9fc32d49e3932279c45118
twoo0220 <twoo0220@gmail.com>
2025-01-14 01:19:56 +0900

    25.01.14 - string

---
+++ BOJ/C++/1032.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int size = 0;
+    std::cin >> size;
+
+    std::string result;
+    std::cin >> result;
+    for (int i = 1; i < size; ++i)
+    {
+        std::string temp;
+        std::cin >> temp;
+
+        for (int j = 0; j < temp.size(); ++j)
+        {
+            if (result[j] != temp[j])
+            {
+                result[j] = '?';
+            }
+        }
+    }
+
+    std::cout << result;
+    return 0;
+}
