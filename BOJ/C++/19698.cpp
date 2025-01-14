4f5083fde7bacbd10236ba25159d99c79aa7c3e8
twoo0220 <twoo0220@gmail.com>
2025-01-15 00:33:20 +0900

    25.01.15 - arithmetic

---
+++ BOJ/C++/19698.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int n(0), w(0), h(0), l(0);
+    std::cin >> n >> w >> h >> l;
+	std::cout << std::min(n, (w / l) * (h / l));
+
+    return 0;
+}
