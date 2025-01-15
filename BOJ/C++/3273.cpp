4d09c2a995460831c4754b92ae830f9afc76cbb8
twoo0220 <twoo0220@gmail.com>
2025-01-16 01:39:11 +0900

    25.01.16 - math, pointer, sort

---
+++ BOJ/C++/3273.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cout.tie(0);
+    std::cin.tie(0);
+
+    int size = 0;
+    std::cin >> size;
+    std::vector<int> arr(size, 0);
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cin >> arr[i];
+    }
+
+	std::sort(arr.begin(), arr.end());
+    int sum(0), result(0);
+    std::cin >> sum;
+
+    for (int i = 0, j = size - 1; i < j;)
+    {
+        int temp = arr[i] + arr[j];
+        if (temp == sum)
+        {
+            ++i;
+            --j;
+            result++;
+        }
+		else if (temp > sum)
+		{
+			--j;
+		}
+		else // temp < sum
+        {
+            ++i;
+        }
+    }
+
+    std::cout << result;
+    return 0;
+}
