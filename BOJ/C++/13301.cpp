e8fc6c824ae6f8cc6f867c82ddddcc940aea4c32
twoo0220 <twoo0220@gmail.com>
2025-01-18 00:40:17 +0900

    25.01.18 - math, DP

---
+++ BOJ/C++/13301.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	long long count = 0;
+	std::cin >> count;
+
+	long long width(1), height(1);
+	for (long long i = 1; i < count; ++i)
+	{
+		if (width > height)
+		{
+			height += width;
+		}
+		else
+		{
+			width += height;
+		}
+	}
+
+	std::cout << (width + height) * 2;
+	return 0;
+}
