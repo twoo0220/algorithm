faf6b3ba7aa18720481a3ea9a8c0c17e55637b1e
twoo0220 <twoo0220@gmail.com>
2025-01-18 01:07:39 +0900

    25.01.18 - arithmetic

---
+++ BOJ/C++/27959.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	unsigned int count(0), price(0);
+	std::cin >> count >> price;
+	if (count * 100 >= price)
+	{
+		std::cout << "Yes";
+	}
+	else
+	{
+		std::cout << "No";
+	}
+
+	return 0;
+}
