5570b7a672f8b553cf72162a8f9b6a93c09199cd
twoo0220 <twoo0220@gmail.com>
2025-01-19 21:09:57 +0900

    25.01.19 - ad-hoc

---
+++ BOJ/C++/1402.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int a(0), b(0);
+		std::cin >> a >> b;
+		std::cout << "yes\n";
+	}
+	return 0;
+}
