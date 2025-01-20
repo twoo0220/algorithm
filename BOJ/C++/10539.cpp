a0d310f457ccb7844abef7c1a6a32329a38a9229
twoo0220 <twoo0220@gmail.com>
2025-01-21 01:31:28 +0900

    25.01.21 - arithmetic, sequence

---
+++ BOJ/C++/10539.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count(0), sum(0);
+	std::cin >> count;
+
+	for (int i = 1; i <= count; ++i)
+	{
+		int temp = 0;
+		std::cin >> temp;
+		int a = temp * i - sum;
+		std::cout << a << " ";
+		sum += a;
+	}
+	return 0;
+}
