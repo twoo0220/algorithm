7ad50add77c33b2131b3688426837fb4c6667cc6
twoo0220 <twoo0220@gmail.com>
2025-01-22 22:59:57 +0900

    25.01.22 - arithmetic, string, gcd

---
+++ BOJ/C++/14490.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+static int gcd(int a, int b)
+{
+	int temp = a;
+
+	while (b != 0)
+	{
+		temp = a % b;
+		a = b;
+		b = temp;
+	}
+
+	return a;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0);
+	char c = 0;
+	std::cin >> a >> c >> b;
+
+	int gcdValue = gcd(a, b);
+	std::cout << a / gcdValue << c << b / gcdValue;
+	return 0;
+}
