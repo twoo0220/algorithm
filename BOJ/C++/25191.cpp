8c752f5e3ea1456bc8fd54ea6f5fbfc64a588717
twoo0220 <twoo0220@gmail.com>
2025-01-23 01:51:00 +0900

    25.01.23 - arithmetic

---
+++ BOJ/C++/25191.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count(0), a(0), b(0);
+	std::cin >> count >> a >> b;
+
+	int bev = a / 2 + b;
+	std::cout << ((bev > count) ? count : bev);
+
+	return 0;
+}
