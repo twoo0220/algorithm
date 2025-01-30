3e90012ca19752db78336ed26dfd9a4a24e19fe8
twoo0220 <twoo0220@gmail.com>
2025-01-30 20:50:48 +0900

    25.01.30 - arithmetic

---
+++ BOJ/C++/10179.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		double price = 0.0;
+		std::cin >> price;
+		std::cout << std::fixed << std::setprecision(2) << "$" << price * 0.8 << '\n';
+	}
+
+	return 0;
+}
