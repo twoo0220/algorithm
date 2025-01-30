a4317a5d34196c51d7a3a65627e169b6ce88e85d
twoo0220 <twoo0220@gmail.com>
2025-01-31 01:03:07 +0900

    25.01.31 - arithmetic

---
+++ BOJ/C++/5032.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int e(0), f(0), c(0);
+	std::cin >> e >> f >> c;
+
+	int sum = e + f;
+	int result = 0;
+
+	while (sum >= c)
+	{
+		e = sum / c;
+		f = sum % c;
+		sum = e + f;
+
+		result += e;
+	}
+
+	std::cout << result;
+	return 0;
+}
