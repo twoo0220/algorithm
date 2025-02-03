8976c282fbec085f2987257ada34ae00134c08f5
twoo0220 <twoo0220@gmail.com>
2025-02-04 01:45:14 +0900

    25.02.04 - arithmetic

---
+++ BOJ/C++/14470.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0), d(0), e(0);
+	std::cin >> a >> b >> c >> d >> e;
+
+	if (a < 0)
+	{
+		std::cout << std::abs(a) * c + d + b * e;
+	}
+	else
+	{
+		std::cout << (b - a) * e;
+	}
+
+	return 0;
+}
