64c367270b6de3021416804648e290e6e42c4f6d
twoo0220 <twoo0220@gmail.com>
2025-02-06 22:27:41 +0900

    25.02.06 - arithmetic

---
+++ BOJ/C++/1350.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	long long cluster(0), size(0);
+	long long* arr = new long long[50] {};
+
+	std::cin >> count;
+	
+	for (int i = 0; i < count; ++i)
+	{
+		std::cin >> arr[i];
+	}
+
+	std::cin >> cluster;
+
+	for (int i = 0; i < count; ++i)
+	{
+		size += (arr[i] / cluster);
+		if (arr[i] % cluster != 0)
+		{
+			size++;
+		}
+	}
+
+	std::cout << cluster * size;
+	delete[] arr;
+	arr = nullptr;
+
+	return 0;
+}
