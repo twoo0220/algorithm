c59bfeb0da4a2cf14fd282219f542c134ee21cb0
twoo0220 <twoo0220@gmail.com>
2025-02-10 01:57:47 +0900

    25.02.10 - arithmetic

---
+++ BOJ/C++/4892.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int index = 1;
+	while (true)
+	{
+		int number = 0;
+		std::cin >> number;
+
+		if (number == 0)
+		{
+			break;
+		}
+
+		std::cout << index << ". ";
+		number *= 3;
+		if (number % 2 == 0)
+		{
+			std::cout << "even ";
+			number /= 2;
+		}
+		else
+		{
+			std::cout << "odd ";
+			number = (number + 1) / 2;
+		}
+
+		number /= 3;
+		std::cout << number << '\n';
+		index++;
+	}
+	return 0;
+}
