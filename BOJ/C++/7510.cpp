f1543a62567a63f6092dc6676c16ec1074fc7fd8
twoo0220 <twoo0220@gmail.com>
2025-02-11 01:41:59 +0900

    25.02.11 - arithmetic, geometry

---
+++ BOJ/C++/7510.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int a(0), b(0), c(0);
+		std::cin >> a >> b >> c;
+
+		a *= a;
+		b *= b;
+		c *= c;
+
+		std::cout << "Scenario #" << i + 1 << ":" << '\n';
+		if ((a + b == c) || (b + c == a) || (a + c == b))
+		{
+			std::cout << "yes" << '\n' << '\n';
+		}
+		else
+		{
+			std::cout << "no" << '\n' << '\n';
+		}
+	}
+	return 0;
+}
