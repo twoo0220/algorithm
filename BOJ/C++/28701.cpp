c97fb341150cea1142a650f61a766b1340294c84
twoo0220 <twoo0220@gmail.com>
2025-02-23 15:05:56 +0900

    25.02.23 - arithmetic

---
+++ BOJ/C++/28701.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number = 0;
+	std::cin >> number;
+
+	int sum = number * (number + 1) / 2;
+	int sumDouble = sum * sum;
+
+	std::cout << sum << '\n';
+	std::cout << sumDouble << '\n';
+	std::cout << sumDouble << '\n';
+
+	return 0;
+}
