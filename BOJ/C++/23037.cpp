138cc590277a435905a4c968edaaa5b86faf55b5
twoo0220 <twoo0220@gmail.com>
2025-02-24 00:33:33 +0900

    25.02.24 - arithmetic

---
+++ BOJ/C++/23037.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number(0), result(0);
+	std::cin >> number;
+
+	while (number != 0)
+	{
+		int temp = number % 10;
+		result += temp * temp * temp * temp * temp;
+		number /= 10;
+	}
+
+	std::cout << result;
+	return 0;
+}
