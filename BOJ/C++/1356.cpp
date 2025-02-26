a4f2b5f117f5284c43c60b738f1812a49198c3bf
twoo0220 <twoo0220@gmail.com>
2025-02-26 22:38:30 +0900

    25.02.26 - arithmetic, bruth force

---
+++ BOJ/C++/1356.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int number(0), left(1), right(1);
+	bool bCheck = false;
+	std::cin >> number;
+
+	right = number % 10;
+	number /= 10;
+
+	while (number != 0)
+	{
+		int tempNumber = number;
+		left = tempNumber % 10;
+		tempNumber /= 10;
+
+		while (tempNumber != 0)
+		{
+			left *= tempNumber % 10;
+			tempNumber /= 10;
+		}
+
+		if (left == right)
+		{
+			bCheck = true;
+			break;
+		}
+
+		right *= number % 10;
+		number /= 10;
+	}
+
+	if (bCheck)
+	{
+		std::cout << "YES";
+	}
+	else
+	{
+		std::cout << "NO";
+	}
+	return 0;
+}
