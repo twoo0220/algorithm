854d6b34d528e1e3a2526b610436dcb539a1d375
twoo0220 <twoo0220@gmail.com>
2025-02-27 22:24:59 +0900

    25.02.27 - arithmetic

---
+++ BOJ/C++/16204.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int n(0), m(0), k(0);
+	std::cin >> n >> m >> k;
+	std::cout << (m > k ? k : m) + ((n - m) > (n - k) ? (n - k) : (n - m));
+	
+	return 0;
+}
