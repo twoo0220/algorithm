b10bd7dc3f3cff447fca8a8427ac8178a94a822f
twoo0220 <twoo0220@gmail.com>
2025-02-28 17:08:43 +0900

    25.02.28 - math, greedy algorithm, sort

---
+++ BOJ/C++/2217.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+	std::vector<int> vec(size);
+	
+	for (int i = 0; i < size; ++i)
+	{
+		std::cin >> vec[i];
+	}
+
+	std::sort(vec.begin(), vec.end());
+	int maxWeight = vec[size - 1];
+
+	for (int i = size - 1; i >= 0; --i)
+	{
+		int currentWeight = vec[i] * (size - i);
+		if (maxWeight < currentWeight)
+		{
+			maxWeight = currentWeight;
+		}
+	}
+
+	std::cout << maxWeight;
+	return 0;
+}
