fee76c957e74c4679d9b7cae94c182823f634c61
twoo0220 <twoo0220@gmail.com>
2025-03-02 22:52:27 +0900

    25.03.02 - arithmetic

---
+++ BOJ/C++/24568.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int r(0), s(0);
+	std::cin >> r >> s;
+	std::cout << ((8 * r) + (s * 3)) - 28;
+
+	return 0;
+}
