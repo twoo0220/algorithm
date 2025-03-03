69c646d40f4ea9a23ba5c6e3a2e0fcab663a3b56
twoo0220 <twoo0220@gmail.com>
2025-03-03 18:37:59 +0900

    25.03.03 - arithmetic

---
+++ BOJ/C++/18301.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int a(0), b(0), c(0);
+	std::cin >> a >> b >> c;
+	std::cout << (a + 1) * (b + 1) / (c + 1) - 1;
+	return 0;
+}
