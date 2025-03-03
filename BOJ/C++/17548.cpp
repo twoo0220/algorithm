4c64b5950a35fa88d385eaf00202760ac3acbce1
twoo0220 <twoo0220@gmail.com>
2025-03-03 19:50:45 +0900

    25.03.03 - string

---
+++ BOJ/C++/17548.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::string hey;
+	std::cin >> hey;
+	
+	int stringLength = hey.length() - 1;
+	std::cout << 'h';
+	for (int i = 1; i < stringLength; ++i)
+	{
+		std::cout << "ee";
+	}
+	std::cout << 'y';
+	return 0;
+}
