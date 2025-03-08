8b01574bf69185ad97b4e4c5bcadf83e69e51e52
twoo0220 <twoo0220@gmail.com>
2025-03-08 20:37:48 +0900

    25.03.08 - arithmetic

---
+++ BOJ/C++/3062.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int number = 0;
+		std::cin >> number;
+
+		int sum = number;
+		int reverseNumber = 0;
+		while (true)
+		{
+			int temp = number % 10;
+			reverseNumber += temp;
+			number /= 10;
+
+			if (number == 0)
+			{
+				break;
+			}
+
+			reverseNumber *= 10;
+		}
+
+		sum += reverseNumber;
+		number = sum;
+		reverseNumber = 0;
+		while (true)
+		{
+			int temp = number % 10;
+			reverseNumber += temp;
+			number /= 10;
+
+			if (number == 0)
+			{
+				break;
+			}
+
+			reverseNumber *= 10;
+		}
+
+		if (sum == reverseNumber)
+		{
+			std::cout << "YES\n";
+		}
+		else
+		{
+			std::cout << "NO\n";
+		}
+	}
+
+	return 0;
+}
