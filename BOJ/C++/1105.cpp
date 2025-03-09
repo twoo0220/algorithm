4f27bc5452a634695e4ca5ff54ec616531d221da
twoo0220 <twoo0220@gmail.com>
2025-03-10 00:26:54 +0900

    25.03.10 - string

---
+++ BOJ/C++/1105.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	std::string L, R;
+	std::cin >> L >> R;
+
+	int count = 0;
+	unsigned int Lsize = static_cast<unsigned int>(L.size());
+	if (Lsize == R.size())
+	{
+		std::string subStr;
+		for (unsigned int i = 0; i < Lsize; ++i)
+		{
+			if (L[i] == R[i])
+			{
+				subStr += L[i];
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		Lsize = subStr.size();
+		for (unsigned int i = 0; i < Lsize; ++i)
+		{
+			if (subStr[i] == '8')
+			{
+				++count;
+			}
+		}
+	}
+	std::cout << count;
+	return 0;
+}
