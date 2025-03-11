af5bbab382b86cf7cdf01c429fc69b184f1bf820
twoo0220 <twoo0220@gmail.com>
2025-03-12 00:15:06 +0900

    25.03.12 - arithmetic

---
+++ BOJ/C++/29751.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	float w(0), h(0);
+	std::cin >> w >> h;
+	std::cout << std::fixed << std::setprecision(1) << w * h / 2;
+
+	return 0;
+}
