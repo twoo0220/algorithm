2cc89aac2164ff207dcdfd796cb315bffcaa14b2
twoo0220 <twoo0220@gmail.com>
2025-03-12 00:21:30 +0900

    25.03.12 - gcd, lcm

---
+++ BOJ/C++/5347.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+static long long gcd(long long a, long long b)
+{
+	long long c = 0;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+
+	return a;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int size = 0;
+	std::cin >> size;
+
+	for (int i = 0; i < size; ++i)
+	{
+		long long a(0), b(0);
+		std::cin >> a >> b;
+		long long gcdValue = gcd(a, b);
+		std::cout << a * b / gcdValue << "\n";
+	}
+
+	return 0;
+}
