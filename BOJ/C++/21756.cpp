b7dd09b8331d52b3a2652656a793d8fea12f3dca
twoo0220 <twoo0220@gmail.com>
2025-03-22 03:44:27 +0900

    25.03.22 - math

---
+++ BOJ/C++/21756.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	int count = 1;
+	int N = 0;
+	std::cin >> N;
+
+	while (count * 2 <= N)
+	{
+		count *= 2;
+	}
+
+	std::cout << count;
+	return 0;
+}
